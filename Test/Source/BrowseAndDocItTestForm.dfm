36cf6afb5f8f055f83067fc4ba96c7ad02ae0c0c
DGH2112 <davidghoyle@gmail.com>
2006-06-24 12:41:28 +0100

    Added a button for the file tokens

--- Test/Source/BrowseAndDocItTestForm.dfm
+++ Test/Source/BrowseAndDocItTestForm.dfm
@@ -1,9 +1,9 @@
 object frmBrowseAndDocItTestForm: TfrmBrowseAndDocItTestForm
-  Left = 229
-  Top = 180
+  Left = 161
+  Top = 170
   Caption = 'frmBrowseAndDocItTestForm'
-  ClientHeight = 789
-  ClientWidth = 1111
+  ClientHeight = 733
+  ClientWidth = 907
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -12,25 +12,35 @@ object frmBrowseAndDocItTestForm: TfrmBrowseAndDocItTestForm
   Font.Style = []
   OldCreateOrder = False
   Position = poDesktopCenter
+  OnCloseQuery = FormCloseQuery
   OnCreate = FormCreate
   OnDestroy = FormDestroy
   PixelsPerInch = 96
   TextHeight = 13
+  object Splitter1: TSplitter
+    Left = 689
+    Top = 43
+    Height = 690
+    ResizeStyle = rsUpdate
+    ExplicitHeight = 746
+  end
   object Panel1: TPanel
-    Left = 0
-    Top = 0
-    Width = 417
-    Height = 789
+    Left = 273
+    Top = 43
+    Width = 416
+    Height = 690
     Align = alLeft
     Caption = 'Panel1'
     TabOrder = 0
+    ExplicitHeight = 745
   end
   object SynEdit1: TSynEdit
-    Left = 417
-    Top = 0
-    Width = 694
-    Height = 789
+    Left = 692
+    Top = 43
+    Width = 215
+    Height = 690
     Align = alClient
+    ActiveLineColor = clSkyBlue
     Font.Charset = DEFAULT_CHARSET
     Font.Color = clWindowText
     Font.Height = -13
@@ -44,10 +54,103 @@ object frmBrowseAndDocItTestForm: TfrmBrowseAndDocItTestForm
     Gutter.Font.Style = []
     Gutter.ShowLineNumbers = True
     Highlighter = SynPasSyn1
+    InsertCaret = ctHalfBlock
     Lines.Strings = (
       '')
-    Options = [eoAutoIndent, eoDragDropEditing, eoEnhanceEndKey, eoGroupUndo, eoKeepCaretX, eoScrollPastEof, eoScrollPastEol, eoShowScrollHint, eoShowSpecialChars, eoSmartTabDelete, eoSmartTabs, eoTabIndent, eoTabsToSpaces]
+    Options = [eoAutoIndent, eoDragDropEditing, eoEnhanceEndKey, eoGroupUndo, eoHideShowScrollbars, eoKeepCaretX, eoRightMouseMovesCursor, eoScrollPastEof, eoScrollPastEol, eoShowScrollHint, eoSmartTabDelete, eoSmartTabs, eoTabIndent, eoTabsToSpaces, eoTrimTrailingSpaces]
     OnChange = SynEdit1Change
+    OnStatusChange = SynEdit1StatusChange
+    ExplicitWidth = 419
+    ExplicitHeight = 745
+  end
+  object Panel2: TPanel
+    Left = 0
+    Top = 0
+    Width = 907
+    Height = 43
+    Align = alTop
+    TabOrder = 2
+    ExplicitWidth = 1111
+    object Button1: TButton
+      Left = 8
+      Top = 12
+      Width = 75
+      Height = 25
+      Caption = 'Tokens'
+      TabOrder = 0
+      OnClick = Button1Click
+    end
+    object CheckBox1: TCheckBox
+      Left = 96
+      Top = 14
+      Width = 153
+      Height = 17
+      Caption = 'Special Characters'
+      TabOrder = 1
+      OnClick = CheckBox1Click
+    end
+    object btnQuit: TButton
+      Left = 216
+      Top = 12
+      Width = 75
+      Height = 25
+      Caption = 'Quit'
+      TabOrder = 2
+      OnClick = btnQuitClick
+    end
+  end
+  object Panel3: TPanel
+    Left = 0
+    Top = 43
+    Width = 273
+    Height = 690
+    Align = alLeft
+    Caption = 'Panel3'
+    TabOrder = 3
+    ExplicitHeight = 745
+    DesignSize = (
+      273
+      690)
+    object DirectoryListBox1: TDirectoryListBox
+      Left = 8
+      Top = 31
+      Width = 259
+      Height = 259
+      Anchors = [akLeft, akTop, akRight]
+      ItemHeight = 16
+      TabOrder = 0
+      OnChange = DirectoryListBox1Change
+    end
+    object DriveComboBox1: TDriveComboBox
+      Left = 8
+      Top = 6
+      Width = 259
+      Height = 19
+      Anchors = [akLeft, akTop, akRight]
+      DirList = DirectoryListBox1
+      TabOrder = 1
+    end
+    object lvFileList: TListView
+      Left = 8
+      Top = 296
+      Width = 259
+      Height = 385
+      Anchors = [akLeft, akTop, akRight, akBottom]
+      Columns = <
+        item
+          Caption = 'Direc'
+          Width = 200
+        end
+        item
+          Caption = 'Errors'
+        end>
+      HideSelection = False
+      ReadOnly = True
+      RowSelect = True
+      TabOrder = 2
+      ViewStyle = vsReport
+      OnClick = FileListClick
+    end
   end
   object SynPasSyn1: TSynPasSyn
     AsmAttri.Foreground = clMaroon
--- Test/Source/BrowseAndDocItTestForm.pas
+++ Test/Source/BrowseAndDocItTestForm.pas
@@ -4,7 +4,7 @@
   and how it can better handle errors.
 
   @Version 1.0
-  @Date    01 Jun 2006
+  @Date    24 Jun 2006
   @Author  David Hoyle
 
 **)
@@ -15,7 +15,7 @@ interface
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, SynEditHighlighter, SynHighlighterPas, SynEdit, ExtCtrls,
-  ModuleExplorerFrame, BaseLanguageModule;
+  ModuleExplorerFrame, BaseLanguageModule, StdCtrls, FileCtrl, ComCtrls;
 
 type
   (** This is thre class that defined the main interface form. **)
@@ -23,16 +23,34 @@ type
     Panel1: TPanel;
     SynEdit1: TSynEdit;
     SynPasSyn1: TSynPasSyn;
+    Panel2: TPanel;
+    Button1: TButton;
+    CheckBox1: TCheckBox;
+    Panel3: TPanel;
+    Splitter1: TSplitter;
+    DirectoryListBox1: TDirectoryListBox;
+    DriveComboBox1: TDriveComboBox;
+    btnQuit: TButton;
+    lvFileList: TListView;
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
     procedure SynEdit1Change(Sender: TObject);
     Procedure SelectionChange(iIdentLine, iIdentCol, iCommentLine,
       iCommentCol : Integer);
+    procedure Button1Click(Sender: TObject);
+    procedure CheckBox1Click(Sender: TObject);
+    procedure SynEdit1StatusChange(Sender: TObject; Changes: TSynStatusChanges);
+    procedure FormCloseQuery(Sender: TObject; var CanClose: Boolean);
+    procedure FileListClick(Sender: TObject);
+    procedure btnQuitClick(Sender: TObject);
+    procedure DirectoryListBox1Change(Sender: TObject);
   private
     { Private declarations }
     FModuleExplorerFrame : TframeModuleExplorer;
     FDocOptions : TDocOptions;
-    FFileName : String;
+    function GetFileName: String;
+    procedure SetFileName(const Value: String);
+    Property FileName : String Read GetFileName Write SetFileName;
   public
     { Public declarations }
   end;
@@ -45,9 +63,171 @@ var
 implementation
 
 Uses
-  PascalDocModule;
+  PascalDocModule, TokenForm, Registry;
 
 {$R *.dfm}
+
+(**
+
+  This is an on click event handler for the Tokens button.
+
+  @precon  None.
+  @postcon Displays a form showing the tokens from the current file.
+
+  @param   Sender as a TObject
+
+**)
+procedure TfrmBrowseAndDocItTestForm.Button1Click(Sender: TObject);
+
+Var
+  M : TPascalDocModule;
+  Source : TMemoryStream;
+
+begin
+  Source := TMemoryStream.Create;
+  Try
+    SynEdit1.Lines.SaveToStream(Source);
+    Source.Position := 0;
+    M := TPascalDocModule.Create(Source, FileName, True,
+      [moParse, moCheckForDocumentConflicts], FDocOptions);
+    Try
+      TfrmTokenForm.Execute(M);
+    Finally
+      M.Free;
+    End;
+  Finally
+    Source.Free;
+  End;
+end;
+
+(**
+
+  This is an on click event handler for the Quit Button.
+
+  @precon  None.
+  @postcon Closes the application.
+
+  @param   Sender as a TObject
+
+**)
+procedure TfrmBrowseAndDocItTestForm.btnQuitClick(Sender: TObject);
+begin
+  Close;
+end;
+
+(**
+
+  This is an on click event handler for the Checkbox1 control.
+
+  @precon  None.
+  @postcon Displays or hides the special character markers in the SynEdit1
+           control.
+
+  @param   Sender as a TObject
+
+**)
+procedure TfrmBrowseAndDocItTestForm.CheckBox1Click(Sender: TObject);
+begin
+  If CheckBox1.Checked Then
+    SynEdit1.Options := SynEdit1.Options + [eoShowSpecialChars]
+  Else
+    SynEdit1.Options := SynEdit1.Options - [eoShowSpecialChars];
+end;
+
+(**
+
+  This is an on chnage event handler for the directory list control.
+
+  @precon  None.
+  @postcon Adds a list of PAS files and the number of errors to the list box.
+
+  @param   Sender as a TObject
+
+**)
+procedure TfrmBrowseAndDocItTestForm.DirectoryListBox1Change(Sender: TObject);
+
+Var
+  recFile : TSearchRec;
+  iResult : Integer;
+  liItem : TListItem;
+  Source : TFileStream;
+  M : TPascalDocModule;
+
+begin
+  iResult := FindFirst(DirectoryListBox1.Directory + '\*.pas', faAnyFile, recFile);
+  Try
+    While iResult = 0 Do
+      Begin
+        liItem := lvFileList.Items.Add;
+        liItem.Caption := recFile.Name;
+        Source := TFileStream.Create(DirectoryListBox1.Directory + '\' +
+          recFile.Name, fmOpenRead);
+        Try
+          Source.Position := 0;
+          M := TPascalDocModule.Create(Source,
+            DirectoryListBox1.Directory + '\' + recFile.Name, False,
+            [moParse], FDocOptions);
+          Try
+            liItem.SubItems.Add(Format('%d', [M.Errors.Count]));
+            Application.ProcessMessages;
+          Finally
+            M.Free;
+          End;
+        Finally
+          Source.Free;
+        End;
+        iResult := FindNext(recFile);
+      End;
+  Finally
+    FindClose(recFile);
+  End;
+end;
+
+(**
+
+  This is an on click event handler for the FileListBox1 control.
+
+  @precon  None.
+  @postcon Sets the filename property to the selected file.
+
+  @param   Sender as a TObject
+
+**)
+procedure TfrmBrowseAndDocItTestForm.FileListClick(Sender: TObject);
+begin
+  If lvFileList.Selected <> Nil Then
+    If FileExists(lvFileList.Selected.Caption) Then
+      FileName := lvFileList.Selected.Caption
+    Else
+      MessageDlg(Format('The file "%s" was not found.',
+        [lvFileList.Selected.Caption]), mtError, [mbOK], 0);
+end;
+
+(**
+
+  This is an on close event handler for the main form.
+
+  @precon  None.
+  @postcon Prompts to save the file if the file has been modified.
+
+  @param   Sender   as a TObject
+  @param   CanClose as a Boolean as a reference
+
+**)
+procedure TfrmBrowseAndDocItTestForm.FormCloseQuery(Sender: TObject;
+  var CanClose: Boolean);
+
+Const
+  strMsg = 'The file "%s" has been modified. Do you want to save the changes?';
+
+begin
+  If SynEdit1.Modified Then
+    Case MessageDlg(Format(strMsg, [FileName]), mtConfirmation,
+      [mbYes, mbNo, mbCancel], 0) Of
+      mrYes: SynEdit1.Lines.SaveToFile(FileName);
+      mrCancel: CanClose := False;
+    End;
+end;
 
 (**
 
@@ -73,9 +253,15 @@ begin
   FModuleExplorerFrame.Parent := Panel1;
   FModuleExplorerFrame.Align := alClient;
   FModuleExplorerFrame.OnSelectionChange := SelectionChange;
-  FFileName := ExtractFilePath(ParamStr(0)) + 'Test Units\Test File.pas';
-  SynEdit1.Lines.LoadFromFile(FFileName);
-  SynEdit1Change(Self);
+  With TRegIniFile.Create Do
+    Try
+      Top := ReadInteger(strRegRootKey + 'Position', 'Top', Top);
+      Left := ReadInteger(strRegRootKey + 'Position', 'Left', Left);
+      Height := ReadInteger(strRegRootKey + 'Position', 'Height', Height);
+      Width := ReadInteger(strRegRootKey + 'Position', 'Width', Width);
+    Finally
+      Free;
+    End;
 end;
 
 (**
@@ -89,10 +275,39 @@ end;
 
 **)
 procedure TfrmBrowseAndDocItTestForm.FormDestroy(Sender: TObject);
+
+Const
+  strMsg = 'The file "%s" has been modified. Do you want to save the changes?';
+
 begin
-  SynEdit1.Lines.SaveToFile(FFileName);
+  With TRegIniFile.Create Do
+    Try
+      WriteInteger(strRegRootKey + 'Position', 'Top', Top);
+      WriteInteger(strRegRootKey + 'Position', 'Left', Left);
+      WriteInteger(strRegRootKey + 'Position', 'Height', Height);
+      WriteInteger(strRegRootKey + 'Position', 'Width', Width);
+    Finally
+      Free;
+    End;
   FModuleExplorerFrame.Free;
   SpecialTags.Free;
+end;
+
+(**
+
+  This is a getter method for the FileName property.
+
+  @precon  None.
+  @postcon Sets the returns the file name property based on the file list box
+           selection.
+
+  @return  a String
+
+**)
+function TfrmBrowseAndDocItTestForm.GetFileName: String;
+begin
+  If lvFileList.Selected <> Nil Then
+    Result := lvFileList.Selected.Caption;
 end;
 
 (**
@@ -110,10 +325,28 @@ end;
 **)
 procedure TfrmBrowseAndDocItTestForm.SelectionChange(iIdentLine, iIdentCol, iCommentLine,
   iCommentCol: Integer);
+
 begin
   SynEdit1.CaretX := iIdentCol;
   SynEdit1.CaretY := iIdentLine;
-  SynEdit1.TopLine := iIdentLine;
+  SynEdit1.TopLine := iIdentLine - SynEdit1.LinesInWindow Div 2;
+end;
+
+(**
+
+  This is a setter method for the FileName property.
+
+  @precon  None.
+  @postcon Sets the SynEdit file to the file named in the FileName property.
+
+  @param   Value as a String constant
+
+**)
+procedure TfrmBrowseAndDocItTestForm.SetFileName(const Value: String);
+begin
+  SynEdit1.Lines.LoadFromFile(FileName);
+  Synedit1.Modified := False;
+  SynEdit1Change(Self);
 end;
 
 (**
@@ -137,7 +370,7 @@ begin
   Try
     SynEdit1.Lines.SaveToStream(Source);
     Source.Position := 0;
-    M := TPascalDocModule.Create(Source, FFileName, True,
+    M := TPascalDocModule.Create(Source, FileName, SynEdit1.Modified,
       [moParse, moCheckForDocumentConflicts], FDocOptions);
     Try
       FModuleExplorerFrame.RenderModule(M, FDocOptions);
@@ -147,6 +380,23 @@ begin
   Finally
     Source.Free;
   End;
+end;
+
+(**
+
+  This is an on status change event handler for the editor.
+
+  @precon  None.
+  @postcon Updates the top panels with the cursor position.
+
+  @param   Sender  as a TObject
+  @param   Changes as a TSynStatusChanges
+
+**)
+procedure TfrmBrowseAndDocItTestForm.SynEdit1StatusChange(Sender: TObject;
+  Changes: TSynStatusChanges);
+begin
+  Panel2.Caption := Format('Line %d, Column %d', [SynEdit1.CaretY, SynEdit1.CaretX]);
 end;
 
 end.

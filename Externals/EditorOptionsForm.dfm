611c976dfc8ba0a513e7a802a5718856d0ad921c
DGH2112 <davidghoyle@gmail.com>
2006-10-14 10:37:04 +0100

    Initial import into the Version Control System.

---
+++ Externals/EditorOptionsForm.dfm
@@ -0,0 +1,387 @@
+object frmEditorOptions: TfrmEditorOptions
+  Left = 493
+  Top = 291
+  BorderStyle = bsDialog
+  Caption = 'Editor Options'
+  ClientHeight = 355
+  ClientWidth = 323
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  Position = poMainFormCenter
+  DesignSize = (
+    323
+    355)
+  PixelsPerInch = 96
+  TextHeight = 13
+  object PageControl1: TPageControl
+    Left = 8
+    Top = 8
+    Width = 307
+    Height = 308
+    ActivePage = VisualTab
+    Anchors = [akLeft, akTop, akRight, akBottom]
+    TabOrder = 0
+    object VisualTab: TTabSheet
+      Caption = 'Visual'
+      ExplicitLeft = 0
+      ExplicitTop = 0
+      ExplicitWidth = 0
+      ExplicitHeight = 0
+      object lblActiveLineColour: TLabel
+        Left = 3
+        Top = 34
+        Width = 86
+        Height = 13
+        Caption = '&Active Line Colour'
+        FocusControl = cbxActiveLineColour
+      end
+      object lblEditorFontName: TLabel
+        Left = 3
+        Top = 62
+        Width = 82
+        Height = 13
+        Caption = 'Editor Font &Name'
+        FocusControl = cbxFontName
+      end
+      object lblEditorFontSize: TLabel
+        Left = 3
+        Top = 89
+        Width = 74
+        Height = 13
+        Caption = 'Editor Font &Size'
+        FocusControl = edtFontSize
+      end
+      object lblRightEdgePosition: TLabel
+        Left = 3
+        Top = 116
+        Width = 93
+        Height = 13
+        Caption = '&Right Edge Position'
+        FocusControl = edtRightEdge
+      end
+      object lblRightEdgeColour: TLabel
+        Left = 3
+        Top = 143
+        Width = 86
+        Height = 13
+        Caption = 'Right &Edge Colour'
+        FocusControl = cbxRightEdgeColour
+      end
+      object lblForeColour: TLabel
+        Left = 3
+        Top = 171
+        Width = 123
+        Height = 13
+        Caption = 'Selected Text &Fore Colour'
+        FocusControl = cbxSelectedForeground
+      end
+      object lblBackColour: TLabel
+        Left = 3
+        Top = 199
+        Width = 127
+        Height = 13
+        Caption = 'Selected &Text Back Colour'
+        FocusControl = cbxSelectedBackground
+      end
+      object lblEditorBackgroundColour: TLabel
+        Left = 3
+        Top = 6
+        Width = 121
+        Height = 13
+        Caption = 'Editor &Background Colour'
+        FocusControl = cbxEditorBackgroundColour
+      end
+      object lblTabWidth: TLabel
+        Left = 3
+        Top = 227
+        Width = 97
+        Height = 13
+        Caption = 'Tab &width in Spaces'
+        FocusControl = edtTabWidth
+      end
+      object cbxActiveLineColour: TColorBox
+        Left = 151
+        Top = 31
+        Width = 145
+        Height = 22
+        Style = [cbStandardColors, cbExtendedColors, cbSystemColors, cbIncludeNone, cbIncludeDefault, cbCustomColor, cbPrettyNames]
+        ItemHeight = 16
+        TabOrder = 1
+      end
+      object cbxFontName: TComboBox
+        Left = 151
+        Top = 59
+        Width = 145
+        Height = 21
+        ItemHeight = 0
+        TabOrder = 2
+        Text = 'cbxFontName'
+      end
+      object edtFontSize: TEdit
+        Left = 152
+        Top = 86
+        Width = 129
+        Height = 21
+        TabOrder = 3
+        Text = '6'
+      end
+      object udEditorFontSize: TUpDown
+        Left = 281
+        Top = 86
+        Width = 16
+        Height = 21
+        Associate = edtFontSize
+        Min = 6
+        Max = 72
+        Position = 6
+        TabOrder = 4
+      end
+      object chxLineNumbers: TCheckBox
+        Left = 3
+        Top = 256
+        Width = 293
+        Height = 17
+        Caption = 'S&how Line Numbers in the Editor Gutter'
+        TabOrder = 12
+      end
+      object edtRightEdge: TEdit
+        Left = 152
+        Top = 113
+        Width = 129
+        Height = 21
+        TabOrder = 5
+        Text = '0'
+      end
+      object udRightEdgePosition: TUpDown
+        Left = 281
+        Top = 113
+        Width = 16
+        Height = 21
+        Associate = edtRightEdge
+        Max = 1024
+        TabOrder = 6
+      end
+      object cbxRightEdgeColour: TColorBox
+        Left = 152
+        Top = 140
+        Width = 145
+        Height = 22
+        Style = [cbStandardColors, cbExtendedColors, cbSystemColors, cbIncludeNone, cbIncludeDefault, cbCustomColor, cbPrettyNames]
+        ItemHeight = 16
+        TabOrder = 7
+      end
+      object cbxSelectedForeground: TColorBox
+        Left = 152
+        Top = 168
+        Width = 145
+        Height = 22
+        Style = [cbStandardColors, cbExtendedColors, cbSystemColors, cbIncludeNone, cbIncludeDefault, cbCustomColor, cbPrettyNames]
+        ItemHeight = 16
+        TabOrder = 8
+      end
+      object cbxSelectedBackground: TColorBox
+        Left = 152
+        Top = 196
+        Width = 145
+        Height = 22
+        Style = [cbStandardColors, cbExtendedColors, cbSystemColors, cbIncludeNone, cbIncludeDefault, cbCustomColor, cbPrettyNames]
+        ItemHeight = 16
+        TabOrder = 9
+      end
+      object cbxEditorBackgroundColour: TColorBox
+        Left = 151
+        Top = 3
+        Width = 145
+        Height = 22
+        Style = [cbStandardColors, cbExtendedColors, cbSystemColors, cbIncludeNone, cbIncludeDefault, cbCustomColor, cbPrettyNames]
+        ItemHeight = 16
+        TabOrder = 0
+      end
+      object edtTabWidth: TEdit
+        Left = 152
+        Top = 224
+        Width = 129
+        Height = 21
+        TabOrder = 10
+        Text = '1'
+      end
+      object udTabWidth: TUpDown
+        Left = 281
+        Top = 224
+        Width = 16
+        Height = 21
+        Associate = edtTabWidth
+        Min = 1
+        Max = 8
+        Position = 1
+        TabOrder = 11
+      end
+    end
+    object BehaviourTab: TTabSheet
+      Caption = 'Behaviour'
+      ImageIndex = 1
+      ExplicitLeft = 0
+      ExplicitTop = 0
+      ExplicitWidth = 0
+      ExplicitHeight = 0
+      DesignSize = (
+        299
+        280)
+      object clbOptions: TCheckListBox
+        Left = 3
+        Top = 3
+        Width = 293
+        Height = 274
+        Anchors = [akLeft, akTop, akRight, akBottom]
+        ItemHeight = 13
+        TabOrder = 0
+      end
+    end
+    object SyntaxTab: TTabSheet
+      Caption = 'Syntax Highlighting'
+      ImageIndex = 2
+      ExplicitLeft = 0
+      ExplicitTop = 0
+      ExplicitWidth = 0
+      ExplicitHeight = 0
+      DesignSize = (
+        299
+        280)
+      object lblAttrForeColour: TLabel
+        Left = 143
+        Top = 40
+        Width = 129
+        Height = 13
+        Caption = 'Attribute &Foreground Colour'
+        FocusControl = cbxAttrForeColour
+      end
+      object lblAttrBackColour: TLabel
+        Left = 147
+        Top = 87
+        Width = 133
+        Height = 13
+        Caption = 'Attribute Background &Colour'
+        FocusControl = cbxAttrBackColour
+      end
+      object lblAttributes: TLabel
+        Left = 3
+        Top = 37
+        Width = 44
+        Height = 13
+        Caption = '&Attributes'
+        FocusControl = lbAttributes
+      end
+      object lblHighlighterType: TLabel
+        Left = 3
+        Top = 3
+        Width = 294
+        Height = 21
+        AutoSize = False
+        Caption = 'No Highligher Available'
+        Font.Charset = DEFAULT_CHARSET
+        Font.Color = clNavy
+        Font.Height = -13
+        Font.Name = 'MS Sans Serif'
+        Font.Style = []
+        ParentFont = False
+      end
+      object lbAttributes: TListBox
+        Left = 3
+        Top = 56
+        Width = 134
+        Height = 221
+        Anchors = [akLeft, akTop, akRight, akBottom]
+        ItemHeight = 13
+        TabOrder = 0
+        OnClick = lbAttributesClick
+      end
+      object cbxAttrForeColour: TColorBox
+        Left = 143
+        Top = 59
+        Width = 153
+        Height = 22
+        Style = [cbStandardColors, cbExtendedColors, cbSystemColors, cbIncludeNone, cbIncludeDefault, cbCustomColor, cbPrettyNames]
+        ItemHeight = 16
+        TabOrder = 1
+        OnClick = AttributeChange
+      end
+      object cbxAttrBackColour: TColorBox
+        Left = 143
+        Top = 104
+        Width = 153
+        Height = 22
+        Style = [cbStandardColors, cbExtendedColors, cbSystemColors, cbIncludeNone, cbIncludeDefault, cbCustomColor, cbPrettyNames]
+        ItemHeight = 16
+        TabOrder = 2
+        OnClick = AttributeChange
+      end
+      object grpFontStyles: TGroupBox
+        Left = 143
+        Top = 132
+        Width = 153
+        Height = 145
+        Caption = 'Attribute Font Styles'
+        TabOrder = 3
+        object cbxBold: TCheckBox
+          Left = 8
+          Top = 24
+          Width = 97
+          Height = 17
+          Caption = '&Bold'
+          TabOrder = 0
+          OnClick = AttributeChange
+        end
+        object cbxItalic: TCheckBox
+          Left = 8
+          Top = 48
+          Width = 97
+          Height = 17
+          Caption = '&Italic'
+          TabOrder = 1
+          OnClick = AttributeChange
+        end
+        object cbxUnderlined: TCheckBox
+          Left = 8
+          Top = 72
+          Width = 97
+          Height = 17
+          Caption = '&Underlined'
+          TabOrder = 2
+          OnClick = AttributeChange
+        end
+        object cbxStrikeout: TCheckBox
+          Left = 8
+          Top = 96
+          Width = 97
+          Height = 17
+          Caption = '&Strikeout'
+          TabOrder = 3
+          OnClick = AttributeChange
+        end
+      end
+    end
+  end
+  object btnOK: TBitBtn
+    Left = 240
+    Top = 322
+    Width = 75
+    Height = 25
+    Anchors = [akRight, akBottom]
+    TabOrder = 2
+    Kind = bkOK
+  end
+  object btnCancel: TBitBtn
+    Left = 159
+    Top = 323
+    Width = 75
+    Height = 25
+    Anchors = [akRight, akBottom]
+    TabOrder = 1
+    Kind = bkCancel
+  end
+end
---
+++ Externals/EditorOptionsForm.pas
@@ -0,0 +1,386 @@
+(**
+
+  This module contains a for editing the visual, behavioural and highlighting
+  properties of a currently conifgured TSynEdit editor.
+
+  @Version 1.0
+  @Author  David Hoyle
+  @Date    14 Oct 2006
+
+**)
+unit EditorOptionsForm;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, ComCtrls, StdCtrls, ExtCtrls, Buttons, SynEdit, CheckLst, Contnrs;
+
+type
+  (** This class represents the form interface for editing the editor
+      options. **)
+  TfrmEditorOptions = class(TForm)
+    PageControl1: TPageControl;
+    btnOK: TBitBtn;
+    btnCancel: TBitBtn;
+    VisualTab: TTabSheet;
+    cbxActiveLineColour: TColorBox;
+    lblActiveLineColour: TLabel;
+    BehaviourTab: TTabSheet;
+    cbxFontName: TComboBox;
+    edtFontSize: TEdit;
+    udEditorFontSize: TUpDown;
+    chxLineNumbers: TCheckBox;
+    edtRightEdge: TEdit;
+    udRightEdgePosition: TUpDown;
+    cbxRightEdgeColour: TColorBox;
+    cbxSelectedForeground: TColorBox;
+    cbxSelectedBackground: TColorBox;
+    SyntaxTab: TTabSheet;
+    lblEditorFontName: TLabel;
+    lblEditorFontSize: TLabel;
+    lblRightEdgePosition: TLabel;
+    lblRightEdgeColour: TLabel;
+    lblForeColour: TLabel;
+    lblBackColour: TLabel;
+    clbOptions: TCheckListBox;
+    cbxEditorBackgroundColour: TColorBox;
+    lblEditorBackgroundColour: TLabel;
+    lbAttributes: TListBox;
+    cbxAttrForeColour: TColorBox;
+    cbxAttrBackColour: TColorBox;
+    lblAttrForeColour: TLabel;
+    lblAttrBackColour: TLabel;
+    grpFontStyles: TGroupBox;
+    cbxBold: TCheckBox;
+    cbxItalic: TCheckBox;
+    cbxUnderlined: TCheckBox;
+    cbxStrikeout: TCheckBox;
+    lblTabWidth: TLabel;
+    edtTabWidth: TEdit;
+    udTabWidth: TUpDown;
+    lblAttributes: TLabel;
+    lblHighlighterType: TLabel;
+    procedure lbAttributesClick(Sender: TObject);
+    procedure AttributeChange(Sender: TObject);
+  private
+    { Private declarations }
+    FAttributes : TObjectList;
+  public
+    { Public declarations }
+    Constructor Create(AOwner : TComponent); Override;
+    Destructor Destroy; Override;
+    Class Function Execute(Editor : TSynEdit) : Boolean;
+  end;
+
+implementation
+
+{$R *.dfm}
+
+Type
+  (** This is a private class to hold information about each highlighter
+      attribute. **)
+  TAttribute = Class
+  Private
+    FForeColour : TColor;
+    FBackColour : TColor;
+    FStyle      : TFontStyles;
+    FName       : String;
+  Protected
+  Public
+    Constructor Create(Name : String; Fore, Back : TColor; Style : TFontStyles);
+    (**
+      This property reads and writes the Attribute Name
+      @precon  None
+      @postcon Reads and writes the Attribute Name
+      @return  a String
+    **)
+    Property Name : String Read FName Write FName;
+    (**
+      This property reads and writes the Attribute ForeColour
+      @precon  None
+      @postcon Reads and writes the Attribute ForeColour
+      @return  a TColor
+    **)
+    Property ForeColour : TColor Read FForeColour Write FForeColour;
+    (**
+      This property reads and writes the Attribute BackColour
+      @precon  None
+      @postcon Reads and writes the Attribute BackColour
+      @return  a TColor
+    **)
+    Property BackColour : TColor Read FBackColour Write FBackColour;
+    (**
+      This property reads and writes the Attribute Style
+      @precon  None
+      @postcon Reads and writes the Attribute Style
+      @return  a TFontStyles
+    **)
+    Property Style : TFontStyles Read FStyle Write FStyle;
+  End;
+
+  (** A record to describe descriptions for each TSynEditorOption. **)
+  TSynEditorOptionsRecord = Record
+    Description : String;
+    Value : TSynEditorOption;
+  End;
+
+Const
+  (** A const array of records to place descriptions agains each of the
+      TSynEditorOptions. Use to provide an list of options. **)
+  BehaviouralOptions : Array[Low(TSynEditorOption)..High(TSynEditorOption)] Of
+    TSynEditorOptionsRecord = (
+    (Description : '<Alt> key invokes Code Selection Mode'; Value: eoAltSetsColumnMode),
+    (Description : 'Auto Indent'; Value: eoAutoIndent),
+    (Description : 'Auto Size Max Scroll Width'; Value: eoAutoSizeMaxScrollWidth),
+    (Description : 'Disable Scroll Arrows'; Value: eoDisableScrollArrows),
+    (Description : 'Drag and Drop Editing'; Value: eoDragDropEditing),
+    (Description : 'Drag and Drop Files'; Value: eoDropFiles),
+    (Description : 'Enhanced Home Key'; Value: eoEnhanceHomeKey),
+    (Description : 'Enhanced End key'; Value: eoEnhanceEndKey),
+    (Description : 'Group Undo'; Value: eoGroupUndo),
+    (Description : 'Half Page Scroll'; Value: eoHalfPageScroll),
+    (Description : 'Hide and Show Scroll Bars'; Value: eoHideShowScrollbars),
+    (Description : 'Keep Caret X'; Value: eoKeepCaretX),
+    (Description : 'No Caret'; Value: eoNoCaret),
+    (Description : 'No Selection'; Value: eoNoSelection),
+    (Description : 'Right Mouse Moves Cursor'; Value: eoRightMouseMovesCursor),
+    (Description : 'Scroll By One Less'; Value: eoScrollByOneLess),
+    (Description : 'Scroll Hint Follows'; Value: eoScrollHintFollows),
+    (Description : 'Scroll Past End of File'; Value: eoScrollPastEof),
+    (Description : 'Scroll Past End of Line'; Value: eoScrollPastEol),
+    (Description : 'Show Scroll Hints'; Value: eoShowScrollHint),
+    (Description : 'Show Special Characters'; Value: eoShowSpecialChars),
+    (Description : 'Smart Tab Delete'; Value: eoSmartTabDelete),
+    (Description : 'Smart Tabs'; Value: eoSmartTabs),
+    (Description : 'Special Line Default FG'; Value: eoSpecialLineDefaultFg),
+    (Description : 'Tab Indent'; Value: eoTabIndent),
+    (Description : 'Tabs to Spaces'; Value: eoTabsToSpaces),
+    (Description : 'Trim Trailing Spaces'; Value: eoTrimTrailingSpaces)
+  );
+
+(**
+
+  This is the constructor method for the TAttribute class.
+
+  @precon  None.
+  @postcon Creates an instance of a TAttribute class.
+
+  @param   Name  as a String
+  @param   Fore  as a TColor
+  @param   Back  as a TColor
+  @param   Style as a TFontStyles
+
+**)
+Constructor TAttribute.Create(Name : String; Fore, Back : TColor;
+  Style : TFontStyles);
+
+Begin
+  FName := Name;
+  FForeColour := Fore;
+  FBackColour := Back;
+  FStyle := Style;
+End;
+
+(**
+
+  This is the constructor method for the TfrmEditorOptions class.
+
+  @precon  None.
+  @postcon Creates an instance of the form and initailises the interface
+           controls.
+
+  @param   AOwner as a TComponent
+
+**)
+Constructor TfrmEditorOptions.Create(AOwner : TComponent);
+
+Var
+  i : Integer;
+  j : TSynEditorOption;
+
+Begin
+  Inherited Create(AOwner);
+  FAttributes := TObjectList.Create(True);
+  For i := 0 To Screen.Fonts.Count - 1 Do
+    cbxFontName.Items.Add(Screen.Fonts[i]);
+  For j := Low(TSynEditorOption) To High(TSynEditorOption) Do
+    clbOptions.Items.Add(BehaviouralOptions[j].Description);
+End;
+
+(**
+
+  This is the destructor method for the TfrmEditorOptions class.
+
+  @precon  None.
+  @postcon Frees the memory used fo the highlighter attributes.
+
+**)
+Destructor TfrmEditorOptions.Destroy;
+
+Begin
+  FAttributes.Free;
+  Inherited Destroy;
+End;
+
+(**
+
+  This is the forms main interface method.
+
+  @precon  Editor must be a valid instance of a TSynEdit cvontrol.
+  @postcon nvokes a form for editing the given instance of the TSynEdit control.
+
+  @param   Editor as a TSynEdit as a reference
+
+**)
+Class Function TfrmEditorOptions.Execute(Editor : TSynEdit) : Boolean;
+
+Var
+  i : TSynEditorOption;
+  j : Integer;
+
+Begin
+  Result := False;
+  With TfrmEditorOptions.Create(Nil) Do
+    Try
+      // Visual
+      udTabWidth.Position := Editor.TabWidth;
+      cbxEditorBackgroundColour.Selected := Editor.Color;
+      cbxActiveLineColour.Selected := Editor.ActiveLineColor;
+      cbxFontName.Text := Editor.Font.Name;
+      udEditorFontSize.Position := Editor.Font.Size;
+      chxLineNumbers.Checked := Editor.Gutter.ShowLineNumbers;
+      udRightEdgePosition.Position := Editor.RightEdge;
+      cbxRightEdgeColour.Selected := Editor.RightEdgeColor;
+      cbxSelectedForeground.Selected := Editor.SelectedColor.Foreground;
+      cbxSelectedBackground.Selected := Editor.SelectedColor.Background;
+      // Behavioural
+       For i := Low(TSynEditorOption) To High(TsynEditorOption) Do
+        clbOptions.Checked[Integer(i)] := i In Editor.Options;
+      // Highlighter
+      If Editor.Highlighter <> Nil Then
+        Begin
+          lblHighlighterType.Caption := Editor.Highlighter.LanguageName;
+          For j := 0 To Editor.Highlighter.AttrCount - 1 Do
+            With Editor.Highlighter.Attribute[j] Do
+              Begin
+                FAttributes.Add(TAttribute.Create(Name, Foreground, Background,
+                  Style));
+                lbAttributes.Items.Add(Editor.Highlighter.Attribute[j].Name);
+              End;
+        End Else
+          SyntaxTab.Enabled := False;
+      // Initialise the Highlighter Attributes.
+      If lbAttributes.Items.Count > 0 Then
+        Begin
+          lbAttributes.ItemIndex := 0;
+          lbAttributesClick(Nil);
+        End;
+      If ShowModal = mrOK Then
+        Begin
+          // Visual
+          Editor.TabWidth := udTabWidth.Position;
+          Editor.Color := cbxEditorBackgroundColour.Selected;
+          Editor.ActiveLineColor := cbxActiveLineColour.Selected;
+          Editor.Font.Name := cbxFontName.Text;
+          Editor.Font.Size := udEditorFontSize.Position;
+          Editor.Gutter.Font.Name := cbxFontName.Text;
+          Editor.Gutter.Font.Size := udEditorFontSize.Position;
+          Editor.Gutter.ShowLineNumbers := chxLineNumbers.Checked;
+          Editor.RightEdge := udRightEdgePosition.Position;
+          Editor.RightEdgeColor := cbxRightEdgeColour.Selected;
+          Editor.SelectedColor.Foreground := cbxSelectedForeground.Selected;
+          Editor.SelectedColor.Background := cbxSelectedBackground.Selected;
+          // Behavioural
+           For i := Low(TSynEditorOption) To High(TsynEditorOption) Do
+            If clbOptions.Checked[Integer(i)] Then
+              Editor.Options := Editor.Options + [i]
+            Else
+              Editor.Options := Editor.Options - [i];
+          // Highlighter
+          If Editor.Highlighter <> Nil Then
+            For j := 0 To Editor.Highlighter.AttrCount - 1 Do
+              With Editor.Highlighter.Attribute[j] Do
+                Begin
+                  Foreground := (FAttributes[j] As TAttribute).ForeColour;
+                  Background := (FAttributes[j] As TAttribute).BackColour;
+                  Style      := (FAttributes[j] As TAttribute).Style;
+                End;
+          Result := True;
+        End;
+    Finally
+      Free;
+    End;
+End;
+
+(**
+
+  This is the attribute list box's on click event handler.
+
+  @precon  None.
+  @postcon Updates the attribute conotrols with the selected attribute in the
+           list box.
+
+  @param   Sender as a TObject
+
+**)
+procedure TfrmEditorOptions.lbAttributesClick(Sender: TObject);
+
+Var
+  i : Integer;
+
+begin
+  If lbAttributes.ItemIndex > -1 Then
+    For i :=  0 To FAttributes.Count - 1 Do
+      With (FAttributes[i] As TAttribute) Do
+      If Name = lbAttributes.Items[lbAttributes.ItemIndex] Then
+        Begin
+          cbxAttrForeColour.Selected := ForeColour;
+          cbxAttrBackColour.Selected := BackColour;
+          cbxBold.Checked := fsBold In Style;
+          cbxItalic.Checked := fsItalic In Style;
+          cbxUnderlined.Checked := fsUnderline In Style;
+          cbxStrikeout.Checked := fsStrikeOut In Style;
+        End;
+end;
+
+(**
+
+  This method is an on changes event handler for the attribute controls.
+
+  @precon  None.
+  @postcon Updates the attribute with the changes in the attribute controls.
+
+  @param   Sender as a TObject
+
+**)
+procedure TfrmEditorOptions.AttributeChange(Sender: TObject);
+
+Var
+  i : Integer;
+
+begin
+  If lbAttributes.ItemIndex > -1 Then
+    For i :=  0 To FAttributes.Count - 1 Do
+      If (FAttributes[i] As TAttribute).Name =
+        lbAttributes.Items[lbAttributes.ItemIndex] Then
+        Begin
+          (FAttributes[i] As TAttribute).ForeColour := cbxAttrForeColour.Selected;
+          (FAttributes[i] As TAttribute).BackColour := cbxAttrBackColour.Selected;
+          (FAttributes[i] As TAttribute).Style := [];
+          If cbxBold.Checked Then
+            (FAttributes[i] As TAttribute).Style :=
+              (FAttributes[i] As TAttribute).Style + [fsBold];
+          If cbxItalic.Checked Then
+            (FAttributes[i] As TAttribute).Style :=
+              (FAttributes[i] As TAttribute).Style + [fsItalic];
+          If cbxUnderlined.Checked Then
+            (FAttributes[i] As TAttribute).Style :=
+              (FAttributes[i] As TAttribute).Style + [fsUnderline];
+          If cbxStrikeout.Checked Then
+            (FAttributes[i] As TAttribute).Style :=
+              (FAttributes[i] As TAttribute).Style + [fsStrikeout];
+        End;
+end;
+
+end.

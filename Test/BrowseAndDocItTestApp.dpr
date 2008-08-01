bf5741a3ca4ad4cfb5cce7a89d31d1198c5bba3c
DGH2112 <davidghoyle@gmail.com>
2008-08-01 22:10:28 +0100

    Updated Main form interface and add enabling/disabling support to the application for memory leaks in FastMem and EurekaLog.

--- Test/BrowseAndDocItTestApp.dpr
+++ Test/BrowseAndDocItTestApp.dpr
@@ -1,31 +1,35 @@
 (**
-  
+
   This is the test applications project file.
 
-  @Date    22 Jul 2008
+  @Date    01 Aug 2008
   @Author  David Hoyle
   @Version 1.0
-  
+
 **)
 program BrowseAndDocItTestApp;
 
 {$R '..\ExplorerImages.res' '..\ExplorerImages.RC'}
+{%TogetherDiagram 'ModelSupport_BrowseAndDocItTestApp\default.txaPackage'}
 
 uses
+  ExceptionLog,
   Forms,
   BrowseAndDocItTestForm in 'Source\BrowseAndDocItTestForm.pas' {frmBrowseAndDocItTestForm},
   SpecialTagForm in '..\Source\SpecialTagForm.pas' {frmSpecialTag},
   BaseLanguageModule in '..\..\..\Library\BaseLanguageModule.pas',
   ModuleExplorerFrame in '..\..\..\Library\ModuleExplorerFrame.pas' {frameModuleExplorer: TFrame},
   OptionsForm in '..\Source\OptionsForm.pas' {frmOptions},
-  PascalDocChecker in '..\..\..\Library\PascalDocChecker.pas',
   PascalDocModule in '..\..\..\Library\PascalDocModule.pas',
   ProgressForm in '..\Source\ProgressForm.pas' {frmProgressForm},
-  TokenForm in '..\..\..\Library\TokenForm.pas' {frmTokenForm};
+  TokenForm in '..\..\..\Library\TokenForm.pas' {frmTokenForm},
+  dghlibrary in '..\..\..\Library\dghlibrary.pas';
 
 {$R *.res}
 
 begin
+  ReportMemoryLeaksOnShutdown := DebugHook <> 0;
+  SetEurekaLogState(DebugHook = 0);
   Application.Initialize;
   Application.CreateForm(TfrmBrowseAndDocItTestForm, frmBrowseAndDocItTestForm);
   Application.Run;

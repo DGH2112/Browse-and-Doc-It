acb7a5c0ac50cb00ab1acc89004383d242a721c7
DGH2112 <davidghoyle@gmail.com>
2008-08-06 18:56:21 +0100

    Initial import into the version control system.

---
+++ Source/ModuleDispatcher.pas
@@ -0,0 +1,59 @@
+(**
+
+  This module containers code to create a module parser from a passed stream
+  and an enumerate for the type of code.
+
+  @Author  David Hoyle
+  @Date    06 Aug 2008
+  @Version 1.0
+
+**)
+Unit ModuleDispatcher;
+
+Interface
+
+Uses
+  SysUtils, Classes, BaseLanguageModule;
+
+Function Dispatcher(Source : TStream; strFileName : String;
+  boolModified : Boolean; ModuleOptions : TModuleOptions) : TBaseLanguageModule;
+
+  Implementation
+
+Uses
+  PascalDocModule;
+
+(**
+
+  This function returns an instance of a TBaseLanguageModule assigned a specific
+  language parser depending on the extension of the file passed.
+
+  @precon  Source must be a valid TStream of charcters to parse.
+  @postcon Returns an instance of a TBaseLanguageModule assigned a specific
+           language parser depending on the extension of the file passed.
+
+  @param   Source        as a TStream
+  @param   strFileName   as a String
+  @param   boolModified  as a Boolean
+  @param   ModuleOptions as a TModuleOptions
+  @return  a TBaseLanguageModule
+
+**)
+Function Dispatcher(Source : TStream; strFileName : String;
+  boolModified : Boolean; ModuleOptions : TModuleOptions) : TBaseLanguageModule;
+
+Var
+  strExt : String;
+
+Begin
+  Result := Nil;
+  strExt := ExtractFileExt(strFileName);
+  If AnsiCompareText(strExt, '.dpk') = 0 Then
+    Result := TPascalModule.Create(Source, strFileName, boolModified, ModuleOptions);
+  If AnsiCompareText(strExt, '.dpr') = 0 Then
+    Result := TPascalModule.Create(Source, strFileName, boolModified, ModuleOptions);
+  If AnsiCompareText(strExt, '.pas') = 0 Then
+    Result := TPascalModule.Create(Source, strFileName, boolModified, ModuleOptions);
+End;
+
+End.

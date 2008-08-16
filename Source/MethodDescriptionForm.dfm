d82b570548be03bd71ae4f8ee6312b60cfa1d1d6
DGH2112 <davidghoyle@gmail.com>
2008-08-16 18:40:10 +0100

    Initial import into the version control system.

---
+++ Source/MethodDescriptionForm.dfm
@@ -0,0 +1,71 @@
+object frmMethodDescriptions: TfrmMethodDescriptions
+  Left = 0
+  Top = 0
+  Caption = 'Method Descriptions'
+  ClientHeight = 101
+  ClientWidth = 632
+  Color = clBtnFace
+  Constraints.MaxHeight = 135
+  Constraints.MinHeight = 135
+  Constraints.MinWidth = 640
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  DesignSize = (
+    632
+    101)
+  PixelsPerInch = 96
+  TextHeight = 13
+  object lblPattern: TLabel
+    Left = 8
+    Top = 8
+    Width = 36
+    Height = 13
+    Caption = '&Pattern'
+    FocusControl = edtPattern
+  end
+  object lblDescription: TLabel
+    Left = 8
+    Top = 54
+    Width = 53
+    Height = 13
+    Caption = '&Description'
+  end
+  object edtPattern: TEdit
+    Left = 8
+    Top = 27
+    Width = 535
+    Height = 21
+    Anchors = [akLeft, akTop, akRight]
+    TabOrder = 0
+  end
+  object edtDescription: TEdit
+    Left = 8
+    Top = 73
+    Width = 535
+    Height = 21
+    Anchors = [akLeft, akTop, akRight]
+    TabOrder = 1
+  end
+  object btnOK: TBitBtn
+    Left = 549
+    Top = 8
+    Width = 75
+    Height = 25
+    Anchors = [akTop, akRight]
+    TabOrder = 2
+    Kind = bkOK
+  end
+  object btnCancel: TBitBtn
+    Left = 549
+    Top = 39
+    Width = 75
+    Height = 25
+    Anchors = [akTop, akRight]
+    TabOrder = 3
+    Kind = bkCancel
+  end
+end
---
+++ Source/MethodDescriptionForm.pas
@@ -0,0 +1,73 @@
+(**
+
+  This module contains a class to add and edit method descriptions.
+
+  @Author  David Hoyle
+  @Version 1.0
+  @Date    16 Aug 2008
+
+**)
+unit MethodDescriptionForm;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, Buttons;
+
+type
+  (** A class to represent the form interface. **)
+  TfrmMethodDescriptions = class(TForm)
+    lblPattern: TLabel;
+    edtPattern: TEdit;
+    lblDescription: TLabel;
+    edtDescription: TEdit;
+    btnOK: TBitBtn;
+    btnCancel: TBitBtn;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+    Class Function Execute(var strPattern, strDescription : String) : Boolean;
+  end;
+
+implementation
+
+{$R *.dfm}
+
+{ TfrmMethodDescriptions }
+
+(**
+
+  This is the forms main interface method for getting the pattern and
+  description.
+
+  @precon  None.
+  @postcon If the dialogue is confirmed then the pattern and description are
+           returned in the var parameters and the function retuen true.
+
+  @param   strPattern     as a String as a reference
+  @param   strDescription as a String as a reference
+  @return  a Boolean
+
+**)
+class function TfrmMethodDescriptions.Execute(var strPattern,
+  strDescription: String): Boolean;
+begin
+  Result := False;
+  With TfrmMethodDescriptions.Create(Nil) Do
+    Try
+      edtPattern.Text := strPattern;
+      edtDescription.Text := strDescription;
+      If ShowModal = mrOK Then
+        Begin
+          strPattern := edtPattern.Text;
+          strDescription := edtDescription.Text;
+          Result := True;
+        End;
+    Finally
+      Free;
+    End;
+end;
+
+end.

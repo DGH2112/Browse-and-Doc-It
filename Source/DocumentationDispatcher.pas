b4813a9f56241d5892f4871095ab4eba57292c3a
DGH2112 <davidghoyle@gmail.com>
2008-09-06 20:48:30 +0100

    Updated Dispatcher

--- Source/DocumentationDispatcher.pas
+++ Source/DocumentationDispatcher.pas
@@ -4,7 +4,7 @@
   that should be produced.
 
   @Author  David Hoyle
-  @Date    05 Sep 2008
+  @Date    06 Sep 2008
   @Version 1.0
 
 **)
@@ -18,19 +18,19 @@ Uses
 Type
   (** This is an enumerate to define the types of documentation hat can be
       produced **)
-  TDocType = (dtHTML {, dtRTF, dtWinHelp, dtHTMLHelp});
+  TDocType = (dtHTML , dtRTF, dtWinHelp, dtHTMLHelp);
 
 Const
   (** This is a constant array to define text representations of the document
       types available. **)
   strDocumentationTypes : Array[Low(TDocType)..High(TDocType)] Of String = (
-    'HTML Documentation' {,
-    'Rich Text Format Documentation',
-    'Win32 Help Documentation',
-    'HTML Help Documentation' }
+    'HTML Documentation',
+    'Rich Text Format Documentation (Not implemented)',
+    'Win32 Help Documentation (Not implemented)',
+    'HTML Help Documentation (Not implemented)'
   );
 
-  Function DocumentDispatcher(strOutputDirectory : String;
+  Function DocumentDispatcher(strOutputDirectory, strTitle : String;
     ADocType : TDocType) : TBaseDocumentation;
 
 Implementation
@@ -40,19 +40,20 @@ Uses
 
 (**
 
-  This function returns a documentation instance for the type of documentation
-  requested.
+  This function returns a documentation instance for the type of documentation 
+  requested. 
 
-  @precon  None.
-  @postcon Returns a documentation instance for the type of documentation
-           requested.
+  @precon  None. 
+  @postcon Returns a documentation instance for the type of documentation 
+           requested. 
 
   @param   strOutputDirectory as a String
+  @param   strTitle           as a String
   @param   ADocType           as a TDocType
   @return  a TBaseDocumentation
 
 **)
-Function DocumentDispatcher(strOutputDirectory : String;
+Function DocumentDispatcher(strOutputDirectory, strTitle : String;
   ADocType : TDocType) : TBaseDocumentation;
 
 ResourceString
@@ -60,7 +61,7 @@ ResourceString
 
 Begin
   Case ADocType Of
-    dtHTML : Result := THTMLDocumentation.Create(strOutputDirectory);
+    dtHTML : Result := THTMLDocumentation.Create(strOutputDirectory, strTitle);
   Else
     Raise Exception.Create(strUnsupportedDocumentationType);
   End;

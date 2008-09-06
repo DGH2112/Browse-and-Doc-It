2ab9b98a718b8cd26867efcb5edf0e4b12e04e98
DGH2112 <davidghoyle@gmail.com>
2008-09-06 14:41:00 +0100

    Initial import into the version control system.

---
+++ Source/DocumentationOptionsForm.dfm
@@ -0,0 +1,52 @@
+object frmDocumentationOptions: TfrmDocumentationOptions
+  Left = 0
+  Top = 0
+  BorderStyle = bsDialog
+  Caption = 'Documentation Options'
+  ClientHeight = 191
+  ClientWidth = 350
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  Position = poMainFormCenter
+  DesignSize = (
+    350
+    191)
+  PixelsPerInch = 96
+  TextHeight = 13
+  object rgpDocumentationOptions: TRadioGroup
+    Left = 8
+    Top = 8
+    Width = 253
+    Height = 175
+    Anchors = [akLeft, akTop, akRight, akBottom]
+    Caption = 'Documentation Options'
+    TabOrder = 0
+    ExplicitWidth = 438
+    ExplicitHeight = 325
+  end
+  object btnOK: TBitBtn
+    Left = 267
+    Top = 8
+    Width = 75
+    Height = 25
+    Anchors = [akTop, akRight]
+    TabOrder = 1
+    Kind = bkOK
+    ExplicitLeft = 452
+  end
+  object btnCancel: TBitBtn
+    Left = 267
+    Top = 39
+    Width = 75
+    Height = 25
+    Anchors = [akTop, akRight]
+    TabOrder = 2
+    Kind = bkCancel
+    ExplicitLeft = 445
+  end
+end
---
+++ Source/DocumentationOptionsForm.pas
@@ -0,0 +1,67 @@
+(**
+  
+  This module contains a class which represent a form for defining the option
+  for Documentation.
+
+  @Version 1.0
+  @Author  David Hoyle
+  @Date    06 Sep 2008
+
+**)
+unit DocumentationOptionsForm;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, Buttons, ExtCtrls, DocumentationDispatcher;
+
+type
+  (** A class to represent the form interface. **)
+  TfrmDocumentationOptions = class(TForm)
+    rgpDocumentationOptions: TRadioGroup;
+    btnOK: TBitBtn;
+    btnCancel: TBitBtn;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+    Class Function Execute(var ADocType : TDocType) : Boolean;
+  end;
+
+implementation
+
+{$R *.dfm}
+
+{ TfrmDocumentationOptions }
+
+(**
+
+  This is the forms main interface method for invokking the dialogue.
+
+  @precon  None.
+  @postcon Returns the documentation type require in the var parameter if the
+           dialogue is accepted while returning true, else returns false.
+
+  @param   ADocType as a TDocType
+  @return  a Boolean
+
+**)
+class function TfrmDocumentationOptions.Execute(var ADocType: TDocType): Boolean;
+
+begin
+  Result := False;
+  With TfrmDocumentationOptions.Create(Nil) Do
+    Try
+      rgpDocumentationOptions.ItemIndex := Byte(ADocType);
+      If ShowModal = mrOK Then
+        Begin
+          ADocType := TDocType(rgpDocumentationOptions.ItemIndex);
+          Result := True;
+        End;
+    Finally
+      Free;
+    End;
+end;
+
+end.

40caf26b21089c41d3e21e07d5806ecb88e547bb
DGH2112 <davidghoyle@gmail.com>
2008-09-10 20:03:43 +0100

    Allowed the module explorer to have a coloured background.

--- Source/ModuleExplorerFrame.pas
+++ Source/ModuleExplorerFrame.pas
@@ -3,7 +3,7 @@
   This module contains a frame which holds all the functionality of the
   module browser so that it can be independant of the application specifics.
 
-  @Date    20 Aug 2008
+  @Date    10 Sep 2008
   @Author  David Hoyle
   @Version 1.0
 
@@ -196,153 +196,13 @@ type
 implementation
 
 Uses
-  IniFiles, Types, Math, DGHLibrary;
+  IniFiles, Types, Math, DGHLibrary, GenericTokenizer;
 
 {$R *.dfm}
 
 Var
   (** A private variable which is assigned the key words array. **)
   strKeyWords : TKeyWords;
-
-(**
-
-  This function returns the token type for a given character and last token
-  type.
-
-  @precon  Ch is the character for which the token type assessment needs to be
-           taken for and LastToken os the type of the last token as this has an
-           effect on some characters.
-  @postcon Returns the token type for the given character.
-
-  @param   Ch           as a Char
-  @param   LastCharType as a TTokenType
-  @return  a TTokenType
-
-**)
-Function GetTokenType(Ch : Char; LastCharType : TTokenType) : TTokenType;
-
-Begin
-  If ch In [#32, #9] Then
-    Result := ttWhiteSpace
-  Else If ch In ['#', '_', 'a'..'z', 'A'..'Z'] Then
-    Begin
-      If (LastCharType = ttNumber) And (Ch In ['A'..'F', 'a'..'f']) Then
-        Result := ttNumber
-      Else
-        Result := ttIdentifier;
-    End
-  Else If ch In ['$', '0'..'9'] Then
-    Begin
-      Result := ttNumber;
-      If LastCharType = ttIdentifier Then
-        Result := ttIdentifier;
-    End
-  Else If ch In [#10, #13] Then
-    Result := ttLineEnd
-  Else If ch In [''''] Then
-    Result := ttStringLiteral
-  Else If ch In [#0..#255] - ['#', '_', 'a'..'z', 'A'..'Z', '$', '0'..'9'] Then
-    Result := ttSymbol
-  Else
-    Result := ttUnknown;
-End;
-
-(**
-
-  This function returns a string list contains the tokenized representation of
-  the passed string with respect to some basic object pascal grammer.
-
-  @precon   strText si the line of text to be tokenised
-  @postcon  Returns a new string list of the tokenized string
-  @note     The string list returnsed must be destroyed be the calling method.
-
-  @param   strText as a String
-  @return  a TStringList
-
-**)
-Function Tokenize(strText : String) : TStringList;
-
-Type
-  (** State machine for block types. **)
-  TBlockType = (btNoBlock, btStringLiteral);
-
-Const
-  (** Growth size of the token buffer. **)
-  iTokenCapacity = 25;
-
-Var
-  (** Token buffer. **)
-  strToken : String;
-  CurToken : TTokenType;
-  LastToken : TTokenType;
-  BlockType : TBlockType;
-  (** Token size **)
-  iTokenLen : Integer;
-  i : Integer;
-
-Begin
-  Result := TStringList.Create;
-  BlockType := btNoBlock;
-  strToken := '';
-  CurToken := ttUnknown;
-  strToken := '';
-
-  iTokenLen := 0;
-  SetLength(strToken, iTokenCapacity);
-
-  For i := 1 To Length(strText) Do
-    Begin
-      LastToken := CurToken;
-      CurToken := GetTokenType(strText[i], LastToken);
-
-      If (LastToken <> CurToken) Or (CurToken = ttSymbol) Then
-        Begin
-          If ((BlockType In [btStringLiteral]) And (CurToken <> ttLineEnd)) Then
-            Begin
-              Inc(iTokenLen);
-              If iTokenLen > Length(strToken) Then
-                SetLength(strToken, iTokenCapacity + Length(strToken));
-              strToken[iTokenLen] := strText[i];
-            End Else
-            Begin
-              SetLength(strToken, iTokenLen);
-              If iTokenLen > 0 Then
-                Begin
-                  If strKeyWords <> Nil Then
-                    If IsKeyWord(strToken, strKeyWords) Then
-                      LastToken := ttReservedWord;
-                  Result.AddObject(strToken, TObject(LastToken));
-                End;
-             BlockType := btNoBlock;
-             iTokenLen := 1;
-             SetLength(strToken, iTokenCapacity);
-             strToken[iTokenLen] := strText[i];
-            End;
-        End Else
-        Begin
-          Inc(iTokenLen);
-          If iTokenLen > Length(strToken) Then
-            SetLength(strToken, iTokenCapacity + Length(strToken));
-          strToken[iTokenLen] := strText[i];
-        End;
-
-      // Check for string literals
-      If CurToken = ttStringLiteral Then
-        If BlockType = btStringLiteral Then
-          BlockType := btNoBlock
-        Else If BlockType = btNoBlock Then
-          BlockType := btStringLiteral;
-
-    End;
-  If iTokenLen > 0 Then
-    Begin
-      SetLength(strToken, iTokenLen);
-      If strKeyWords <> Nil Then
-        If IsKeyWord(strToken, strKeyWords) Then
-          CurToken := ttReservedWord;
-      Result.AddObject(strToken, TObject(CurToken));
-    End;
-End;
 
 (**
 
@@ -714,7 +574,7 @@ End;
            strStatus is a text string to be displayed in the forms status bar.
   @postcon Renders the module information for the given module.
 
-  @param   M                  as a TBaseLanguageModule
+  @param   M as a TBaseLanguageModule
 
 **)
 procedure TframeModuleExplorer.RenderModule(M : TBaseLanguageModule);
@@ -725,6 +585,7 @@ Var
   strSelection : String;
 
 Begin
+  tvExplorer.Color := BRowseAndDocItOptions.BGColour;
   If M = Nil Then
     strKeyWords := Nil
   Else
@@ -1082,7 +943,7 @@ begin
     If Not DefaultDraw Then
       Begin
         iOffset := GetScrollPos(Sender.Handle, SB_HORZ);
-        sl := Tokenize(Node.Text);
+        sl := Tokenize(Node.Text, strKeyWords);
         Try
           // Highlight selected item.
           If cdsSelected In State Then
@@ -1280,7 +1141,7 @@ Begin
   With Canvas Do
     Begin
       Font.Assign(FTreeView.Font);
-      sl := Tokenize(Caption);
+      sl := Tokenize(Caption, strKeyWords);
       Try
         iPos := 2;
         For i := 0 To sl.Count - 1 Do
@@ -1398,7 +1259,7 @@ Begin
     Begin
       // Need to amend the width of the rectangle for the custom drawing
       iPos := 5;
-      sl := Tokenize(Node.Text);
+      sl := Tokenize(Node.Text, strKeyWords);
       Try
         iLastmax := 0;
         For i := 0 To sl.Count - 1 Do

eaeb49dddff8c1c01ae447ed549ffff1d5e83e70
DGH2112 <davidghoyle@gmail.com>
2008-12-11 20:17:20 +0000

    Updated form to use BrowseAndDocItOptions colours and font styles.

--- Source/TokenForm.pas
+++ Source/TokenForm.pas
@@ -3,7 +3,7 @@
   This is a debug form for displaying the tokens and their information.
 
   @version      0.9
-  @date         13 Aug 2008
+  @date         10 Dec 2008
   @author       David Hoyle
 
 **)
@@ -38,73 +38,6 @@ Uses
 
 (**
 
-  This function returns the colour requires correspondig to the type of token
-  in strText.
-
-  @precon  strText is a string representation of a token type.
-  @postcon Returns the appropriate colour for the token type.
-
-  @param   strText as a String
-  @return  a TColor
-
-**)
-Function GetColour(strText : String) : TColor;
-
-Var
-  i : TTokenType;
-
-Begin
-  Result := clBlack;
-  For i := ttUnknown To ttDirective Do
-    If strText = strTokenType[i] Then
-      Case i Of
-        ttUnKnown : Result := clRed;
-        ttReservedWord, ttDirective : Result := clBlack;
-        ttIdentifier : Result := clNavy;
-        ttSymbol, ttNumber : Result := clGreen;
-        ttStringLiteral : Result := clTeal;
-        ttComment, ttCompilerDirective : Result := clPurple;
-        ttHTMLTag : Result := clBlue;
-      Else
-        Result := clBlack;
-      End;
-End;
-
-(**
-
-  This method returns the requires font style for the type of token.
-
-  @precon  strText is a text representation of a token type.
-  @postcon Returns a font style set for the given token type.
-
-  @param   strText as a String
-  @return  a TFontStyles
-
-**)
-Function GetStyle(strText : String) : TFontStyles;
-
-Var
-  i : TTokenType;
-
-Begin
-  Result := [];
-  For i := ttUnknown To ttDirective Do
-    If strText = strTokenType[i] Then
-      Case i Of
-        ttUnKnown : Result := [];
-        ttReservedWord, ttDirective : Result := [fsBold];
-        ttIdentifier : Result := [];
-        ttSymbol, ttNumber : Result := [fsBold];
-        ttStringLiteral : Result := [fsBold];
-        ttComment, ttCompilerDirective : Result := [fsItalic];
-        ttHTMLTag : Result := [fsBold];
-      Else
-        Result := [];
-      End;
-End;
-
-(**
-
   This is a custom draw method for the TListView component. It colours the
   items differently depending on the contents of the item.caption property.
 
@@ -122,9 +55,14 @@ End;
 **)
 procedure TfrmTokenForm.lvListView1CustomDrawItem(Sender: TCustomListView;
   Item: TListItem; State: TCustomDrawState; var DefaultDraw: Boolean);
+
+Var
+  T : TTokenType;
+
 begin
-  lvListView1.Canvas.Font.Color := GetColour(Item.SubItems[0]);
-  lvListView1.Canvas.Font.Style := GetStyle(Item.SubItems[0]);
+  T := TTokenType(StrToInt(Item.SubItems[6]));
+  lvListView1.Canvas.Font.Color := BrowseAndDocItOptions.TokenFontInfo[T].FColour;
+  lvListView1.Canvas.Font.Style := BrowseAndDocItOptions.TokenFontInfo[T].FStyles;
 end;
 
 (**
@@ -159,6 +97,7 @@ class procedure TfrmTokenForm.Execute(Source: TBaseLanguageModule);
 Var
   i : Integer;
   frm : TfrmProgress;
+  Item : TListItem;
 
 begin
   With TfrmTokenForm.Create(nil) Do
@@ -170,16 +109,15 @@ begin
         Try
           For i := 0 To Source.TokenCount - 1 Do
             Begin
-              With lvListView1.Items.Add Do
-                Begin
-                  Caption := IntToStr(i);
-                  SubItems.Add(strTokenType[Source.TokenInfo[i].TokenType]);
-                  SubItems.Add(IntToStr(Source.TokenInfo[i].BufferPos));
-                  SubItems.Add(IntToStr(Source.TokenInfo[i].Line));
-                  SubItems.Add(IntToStr(Source.TokenInfo[i].Column));
-                  SubItems.Add(IntToStr(Source.TokenInfo[i].Length));
-                  SubItems.Add(Source.TokenInfo[i].Token);
-                End;
+              Item := lvListView1.Items.Add;
+              Item.Caption := IntToStr(i);
+              Item.SubItems.Add(strTokenType[Source.TokenInfo[i].TokenType]);
+              Item.SubItems.Add(IntToStr(Source.TokenInfo[i].BufferPos));
+              Item.SubItems.Add(IntToStr(Source.TokenInfo[i].Line));
+              Item.SubItems.Add(IntToStr(Source.TokenInfo[i].Column));
+              Item.SubItems.Add(IntToStr(Source.TokenInfo[i].Length));
+              Item.SubItems.Add(Source.TokenInfo[i].Token);
+              Item.SubItems.Add(Format('%d', [Integer(Source.TokenInfo[i].TokenType)]));
               If i Mod 10 = 0 Then
                 frm.UpdateProgress(i, Format('Getting Tokens... %d', [i]));
             End;

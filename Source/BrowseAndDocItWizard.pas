d61ce4341a1f9b9635f9f3b507e57666509b0127
DGH2112 <davidghoyle@gmail.com>
2009-07-19 17:11:54 +0100

    Added a custom highlighter for Backus-Naur grammar.

--- Source/BrowseAndDocItWizard.pas
+++ Source/BrowseAndDocItWizard.pas
@@ -3,7 +3,7 @@
   This module contains the packages main wizard interface.
 
   @Author  David Hoyle
-  @Date    17 Jul 2009
+  @Date    19 Jul 2009
   @Version 1.0
 
 **)
@@ -128,6 +128,21 @@ Type
     Constructor Create(Wizard : TBrowseAndDocItWizard);
   End;
 
+  (** A class to define an new IDE Highlighter for BNF Grammar **)
+  TBNFHighlighter = Class(TNotifierObject, IOTAHighlighter)
+  {$IFDEF D2005} Strict {$ENDIF} Private
+  {$IFDEF D2005} Strict {$ENDIF} Protected
+  Public
+    Constructor Create;
+    Destructor Destroy; Override;
+    function GetIDString: string;
+    function GetName: string;
+    procedure Tokenize(StartClass: Byte; LineBuf: PAnsiChar; LineBufLen: Word;
+      HighlightCodes: POTASyntaxCode);
+    function TokenizeLineClass(StartClass: Byte; LineBuf: PAnsiChar;
+      LineBufLen: Word): Byte;
+  End;
+
   Procedure Register;
   Function InitWizard(Const BorlandIDEServices : IBorlandIDEServices;
     RegisterProc : TWizardRegisterProc;
@@ -191,7 +206,9 @@ Var
   (** An index for the keybinding nofitier - required when the package is
       unloaded **)
   iKeyBinding: Integer;
-  {boolCompiling : Boolean;}
+  (** An index for the Highlighter notifier - required for unloading the
+      highlighter. **)
+  iBNFHighlighter : Integer;
 
 (**
 
@@ -223,7 +240,9 @@ Begin
   objEditorNotifier := TEditorNotifier.Create;
   {$ENDIF}
   iKeyBinding := (BorlandIDEServices As IOTAKeyboardServices).AddKeyboardBinding(
-    TKeyboardBinding.Create(Wizard))
+    TKeyboardBinding.Create(Wizard));
+  iBNFHighlighter := (BorlandIDEServices As IOTAHighlightServices).AddHighlighter(
+    TBNFHighlighter.Create);
 End;
 
 (**
@@ -1109,7 +1128,6 @@ Var
   Svcs : IOTAServices;
   Wizard : TBrowseAndDocItWizard;
 
-
 Begin
   Application.Handle := Application.MainForm.Handle;
   TfrmDockableModuleExplorer.CreateDockableModuleExplorer;
@@ -1129,7 +1147,9 @@ Begin
       objEditorNotifier := TEditorNotifier.Create;
       {$ENDIF}
       iKeyBinding := (BorlandIDEServices As IOTAKeyboardServices).AddKeyboardBinding(
-        TKeyboardBinding.Create(Wizard))
+        TKeyboardBinding.Create(Wizard));
+      iBNFHighlighter := (BorlandIDEServices As IOTAHighlightServices).AddHighlighter(
+        TBNFHighlighter.Create);
     End;
 End;
 
@@ -1730,6 +1750,252 @@ Var
   iBuild : Integer;
 {$ENDIF}
 
+
+(**
+
+  A constructor for the TBNFHighlighter class.
+
+  @precon  None.
+  @postcon Should create a set of BNF Edit Options but currently throws an AV.
+
+**)
+constructor TBNFHighlighter.Create;
+
+Var
+  EditOps : IOTAEditOptions;
+  iEditOps : Integer;
+
+begin
+  EditOps := Nil;
+  With (BorlandIDEServices As IOTAEditorServices) Do
+    For iEditOps := 0 To EditOptionsCount - 1 Do
+      If EditorOptions[iEditOps].IDString = 'Backus-Naur' Then
+        EditOps := EditorOptions[iEditOps];
+  If EditOps = Nil Then
+    Begin
+      //: @bug This causes an AV in the IDE - I think this is a bug in RAD Studio 2009.
+      //EditOps := (BorlandIDEServices As IOTAEditorServices).AddEditOptions('Backus-Naur');
+      //EditOps.Extensions := 'bnf';
+      //EditOps.OptionsName := 'Backus-Naur Grammar';
+      //EditOps.SyntaxHighlighter := Self;
+    End;
+end;
+
+(**
+
+  This method returns a unique string ID for the highlighter.
+
+  @precon  None.
+  @postcon Returns a unique string ID for the highlighter.
+
+  @return  a string
+
+**)
+destructor TBNFHighlighter.Destroy;
+begin
+  inherited;
+end;
+
+function TBNFHighlighter.GetIDString: string;
+begin
+  Result := 'DGH.Backus-Naur Grammar Highlighter';
+end;
+
+(**
+
+  This method returns the descriptive name for the highlighter.
+
+  @precon  None.
+  @postcon Returns the descriptive name for the highlighter.
+
+  @return  a string
+
+**)
+function TBNFHighlighter.GetName: string;
+begin
+  Result := 'Backus-Naur Grammar Highlighter';
+end;
+
+(**
+
+  This method returns the higlighter mark up codes for the given Line Buffer.
+
+  @precon  None.
+  @postcon Returns the higlighter mark up codes for the given Line Buffer.
+
+  @param   StartClass     as a Byte
+  @param   LineBuf        as a PAnsiChar
+  @param   LineBufLen     as a Word
+  @param   HighlightCodes as a POTASyntaxCode
+
+**)
+procedure TBNFHighlighter.Tokenize(StartClass: Byte; LineBuf: PAnsiChar;
+  LineBufLen: Word; HighlightCodes: POTASyntaxCode);
+
+Type
+  TBlockType = (btNone, btIdentifier, btSingleLiteral, btDoubleLiteral,
+     btTextDefinition, btLineComment, btBlockComment);
+
+Const
+  strValidSymbols : Set Of AnsiChar = ([':', '=', '(', ')', '[', ']', '+', '*', '|', '''', '"']);
+  strInvalidSymbols : Set Of AnsiChar = ([#33..#255] - ['A'..'Z'] - ['a'..'z'] -
+    ['0'..'9'] - [':', '=', '(', ')', '[', ']', '+', '*', '|', '''', '"']);
+
+Var
+  Codes : PAnsiChar;
+  i : Integer;
+  CurChar, LastCHar : AnsiChar;
+  BlockType : TBlockType;
+  iBlockStart : Integer;
+
+  (**
+
+    This procedure checks for the end of a block type.
+
+    @precon  None.
+    @postcon Checks for the end of a block type and returns the block to btNone
+             if found.
+
+    @param   cChar          as an AnsiChar
+    @param   CheckBlockType as a TBlockType
+
+  **)
+  Procedure CheckBlockEnd(cChar : AnsiChar; CheckBlockType : TBlockType);
+
+  Begin
+    If (LastChar = cChar) And (BlockType = CheckBlockType) And (i > iBlockStart + 1) Then
+      Begin
+        Codes[i] := AnsiChar($E);
+        BlockType := btNone;
+        iBlockStart := 0;
+      End;
+  End;
+
+  (**
+
+    This method checks to the start of a block section and set the block type.
+
+    @precon  None.
+    @postcon Checks to the start of a block section and set the block type.
+
+    @param   cChar          as an AnsiChar
+    @param   CheckBlockType as a TBlockType
+    @param   iAttribute     as an Integer
+    @return  a Boolean
+
+  **)
+  Function CheckBlockStart(cChar : AnsiChar; CheckBlockType : TBlockType;
+    iAttribute : Integer) : Boolean;
+
+  Begin
+    Result := False;
+    If ((CurChar = cChar) And (BlockType = btNone)) Or (BlockType = CheckBlockType) Then
+      Begin
+        Codes[i] := AnsiChar(iAttribute);
+        BlockType := CheckBlockType;
+        If iBlockStart = 0 Then
+          iBlockStart := i;
+        Result := True;
+      End Else
+  End;
+
+begin
+  CurChar := #0;
+  LastChar := #0;
+  BlockType := btNone;
+  iBlockStart := 0;
+  Codes := PAnsiChar(HighlightCodes);
+  FillChar(HighlightCodes^, LineBufLen, $E); // No highlighter
+  For i := 0 To LineBufLen - 1 Do
+    Begin
+      If StartClass = atComment Then
+        Begin
+          Codes[i] := Char(atComment);
+          If LineBuf[i] = '/' Then
+            StartClass := atWhiteSpace;
+        End Else
+        Begin
+          LastChar := CurChar;
+          CurChar := LineBuf[i];
+          If LastChar In ['>'] Then
+            Begin
+              Codes[i] := AnsiChar($E);
+              BlockType := btNone;
+            End;
+          CheckBlockEnd('''', btSingleLiteral);
+          CheckBlockEnd('"', btSingleLiteral);
+          //CheckBlockEnd('/', btLineComment);
+
+          If CheckBlockStart('''', btSingleLiteral, atString) Then
+          Else If CheckBlockStart('"', btDoubleLiteral, atString) Then
+          Else If CheckBlockStart('?', btTextDefinition, atPreproc) Then
+          Else If ((CurChar In ['<']) And (BlockType = btNone)) Or (BlockType = btIdentifier) Then
+            Begin
+              Codes[i] := AnsiChar(atIdentifier);
+              BlockType := btIdentifier;
+            End Else
+          If ((LastChar In ['/']) And (CurChar In ['/'])) Or (BlockType In [btLineComment]) Then
+            Begin
+              Codes[i - 1] := AnsiChar(atComment);
+              Codes[i] := AnsiChar(atComment);
+              BlockType := btLineComment;
+              If iBlockStart = 0 Then
+                iBlockStart := i - 1;
+            End Else
+          If ((LastChar In ['/']) And (CurChar In ['*'])) Or (BlockType In [btBlockComment]) Then
+            Begin
+              Codes[i - 1] := AnsiChar(atComment);
+              Codes[i] := AnsiChar(atComment);
+              BlockType := btBlockComment;
+              If iBlockStart = 0 Then
+                iBlockStart := i - 1;
+              StartClass := atComment;
+            End Else
+          If CurChar In strValidSymbols Then
+            Codes[i] := AnsiChar(atSymbol)
+          Else If CurChar In strInvalidSymbols Then
+            Codes[i] := AnsiChar(atIllegal);
+        End;
+    End;
+end;
+
+(**
+
+  This method returns the highlighter code for the next line in the editor. Used
+  for the block comment.
+
+  @precon  None.
+  @postcon Returns the highlighter code for the next line in the editor. Used
+           for the block comment.
+
+  @param   StartClass as a Byte
+  @param   LineBuf    as a PAnsiChar
+  @param   LineBufLen as a Word
+  @return  a Byte
+
+**)
+function TBNFHighlighter.TokenizeLineClass(StartClass: Byte; LineBuf: PAnsiChar;
+  LineBufLen: Word): Byte;
+
+Var
+  i : Integer;
+  LastChar, CurChar: AnsiChar;
+
+begin
+  Result := StartClass;
+  LastChar := #0;
+  CurChar := #0;
+  For i := 0 To LineBufLen - 1 Do
+    Begin
+      LastChar := CurChar;
+      CurChar := LineBuf[i];
+      If (LastChar In ['/']) And (CurChar In ['*']) Then
+        Result := atComment
+      Else If (LastChar In ['*']) And (CurChar In ['/']) Then
+        Result := atWhiteSpace;
+    End;
+end;
+
 (** This initialization section installs an IDE Splash Screen item. **)
 Initialization
   {$IFDEF D2005}
@@ -1745,6 +2011,7 @@ Initialization
 (** This finalization section removes this wizard from the IDE when the package
     is unloaded. **)
 Finalization
+  (BorlandIDEServices As IOTAHighlightServices).RemoveHighlighter(iBNFHighlighter);
   (BorlandIDEServices As IOTAKeyboardServices).RemoveKeyboardBinding(iKeyBinding);
   {$IFDEF D2005}
   (BorlandIDEServices As IOTAEditorServices).RemoveNotifier(iEditorIndex);

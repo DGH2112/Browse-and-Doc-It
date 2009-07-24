ea4c6c4eb1b32b73cc30385b300e6e04d5eac399
DGH2112 <davidghoyle@gmail.com>
2009-07-24 18:12:37 +0100

    Added a new module for XML.

--- Source/ModuleDispatcher.pas
+++ Source/ModuleDispatcher.pas
@@ -4,7 +4,7 @@
   and an enumerate for the type of code.
 
   @Author  David Hoyle
-  @Date    20 Jul 2009
+  @Date    24 Jul 2009
   @Version 1.0
 
 **)
@@ -25,7 +25,7 @@ Uses
 Implementation
 
 Uses
-  Windows, PascalModule, VBModule, BackusNaurModule;
+  Windows, PascalModule, VBModule, BackusNaurModule, XMLModule;
 
 Type
   (** A class type to define classes in the record structure. **)
@@ -43,21 +43,17 @@ Type
 
 Const
   (** A constant array of file extensions with the appropriate parser modules. **)
-  Modules : Array[1..7] of TDispatcherInfo = (
-    (FExt: '.bas'; FCls: TVBModule        ; FCanDoc: True;
-      FBlockCmt: ctVBLine; FLineCmt: ctVBLine; FInSituCmt: ctVBLine),
-    (FExt: '.bnf'; FCls: TBackusNaurModule; FCanDoc: True;
-      FBlockCmt: ctCPPBlock; FLineCmt: ctCPPBlock; FInSituCmt: ctCPPBlock),
-    (FExt: '.cls'; FCls: TVBModule        ; FCanDoc: True;
-      FBlockCmt: ctVBLine; FLineCmt: ctVBLine; FInSituCmt: ctVBLine),
-    (FExt: '.dpk'; FCls: TPascalModule    ; FCanDoc: True;
-      FBlockCmt: ctPascalBlock; FLineCmt: ctPascalBlock; FInSituCmt: ctPascalBlock),
-    (FExt: '.dpr'; FCls: TPascalModule    ; FCanDoc: True;
-      FBlockCmt: ctPascalBlock; FLineCmt: ctPascalBlock; FInSituCmt: ctPascalBlock),
-    (FExt: '.frm'; FCls: TVBModule        ; FCanDoc: True;
-      FBlockCmt: ctVBLine; FLineCmt: ctVBLine; FInSituCmt: ctVBLine),
-    (FExt: '.pas'; FCls: TPascalModule    ; FCanDoc: True;
-      FBlockCmt: ctPascalBlock; FLineCmt: ctPascalBlock; FInSituCmt: ctPascalBlock)
+  Modules : Array[1..10] of TDispatcherInfo = (
+    (FExt: '.bas';  FCls: TVBModule        ; FCanDoc: True;  FBlockCmt: ctVBLine; FLineCmt: ctVBLine; FInSituCmt: ctVBLine),
+    (FExt: '.bnf';  FCls: TBackusNaurModule; FCanDoc: True;  FBlockCmt: ctCPPBlock; FLineCmt: ctCPPBlock; FInSituCmt: ctCPPBlock),
+    (FExt: '.cls';  FCls: TVBModule        ; FCanDoc: True;  FBlockCmt: ctVBLine; FLineCmt: ctVBLine; FInSituCmt: ctVBLine),
+    (FExt: '.dpk';  FCls: TPascalModule    ; FCanDoc: True;  FBlockCmt: ctPascalBlock; FLineCmt: ctPascalBlock; FInSituCmt: ctPascalBlock),
+    (FExt: '.dpr';  FCls: TPascalModule    ; FCanDoc: True;  FBlockCmt: ctPascalBlock; FLineCmt: ctPascalBlock; FInSituCmt: ctPascalBlock),
+    (FExt: '.frm';  FCls: TVBModule        ; FCanDoc: True;  FBlockCmt: ctVBLine; FLineCmt: ctVBLine; FInSituCmt: ctVBLine),
+    (FExt: '.htm';  FCls: TXMLModule       ; FCanDoc: False; FBlockCmt: ctXML; FLineCmt: ctXML; FInSituCmt: ctXML),
+    (FExt: '.html'; FCls: TXMLModule       ; FCanDoc: False; FBlockCmt: ctXML; FLineCmt: ctXML; FInSituCmt: ctXML),
+    (FExt: '.pas';  FCls: TPascalModule    ; FCanDoc: True;  FBlockCmt: ctPascalBlock; FLineCmt: ctPascalBlock; FInSituCmt: ctPascalBlock),
+    (FExt: '.xml';  FCls: TXMLModule       ; FCanDoc: False; FBlockCmt: ctXML; FLineCmt: ctXML; FInSituCmt: ctXML)
   );
 
 (**

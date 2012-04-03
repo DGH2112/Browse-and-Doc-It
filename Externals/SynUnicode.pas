c3ea3a4dc30bcdaa97bc1195997dc7e33e58f8c6
DGH2112 <davidghoyle@gmail.com>
2012-04-03 16:20:02 +0100

    Updated SynEdit components to their latest version from the SVN repository.

--- Externals/SynUnicode.pas
+++ Externals/SynUnicode.pas
@@ -26,7 +26,7 @@ replace them with the notice and other provisions required by the GPL.
 If you do not delete the provisions above, a recipient may use your version
 of this file under either the MPL or the GPL.
 
-$Id: SynUnicode.pas,v 1.1.2.43 2008/10/03 18:50:12 maelh Exp $
+$Id: SynUnicode.pas,v 1.1.2.46 2009/09/28 17:54:20 maelh Exp $
 
 You may retrieve the latest version of this file at the SynEdit home page,
 located at http://SynEdit.SourceForge.net
@@ -295,6 +295,7 @@ function WStrAlloc(Size: Cardinal): PWideChar;
 function WStrNew(const Str: PWideChar): PWideChar;
 procedure WStrDispose(Str: PWideChar);
 {$ENDIF}
+
 
 {$IFNDEF SYN_COMPILER_6_UP}
 {$IFDEF SYN_WIN32} // Kylix should have that from version 1 on
@@ -855,15 +856,11 @@ procedure TUnicodeStrings.LoadFromFile(const FileName: TFileName);
 var
   Stream: TStream;
 begin
+  Stream := TFileStream.Create(FileName, fmOpenRead or fmShareDenyNone);
   try
-    Stream := TFileStream.Create(FileName, fmOpenRead or fmShareDenyNone);
-    try
-      LoadFromStream(Stream);
-    finally
-      Stream.Free;
-    end;
-  except
-    RaiseLastOSError;
+    LoadFromStream(Stream);
+  finally
+    Stream.Free;
   end;
 end;
 
@@ -1546,6 +1543,10 @@ begin
 end;
 
 function WStrCopy(Dest: PWideChar; const Source: PWideChar): PWideChar;
+{$IFDEF SYN_COMPILER_16_UP}
+begin
+  Result := SysUtils.StrCopy(Dest, Source)
+{$ELSE}
 asm
         PUSH    EDI
         PUSH    ESI
@@ -1566,9 +1567,14 @@ asm
         REP     MOVSW
         POP     ESI
         POP     EDI
+{$ENDIF}
 end;
 
 function WStrLCopy(Dest: PWideChar; const Source: PWideChar; MaxLen: Cardinal): PWideChar;
+{$IFDEF SYN_COMPILER_16_UP}
+begin
+  Result := SysUtils.StrLCopy(Dest, Source, MaxLen)
+{$ELSE}
 asm
         PUSH    EDI
         PUSH    ESI
@@ -1597,6 +1603,7 @@ asm
         POP     EBX
         POP     ESI
         POP     EDI
+{$ENDIF}
 end;
 
 function WStrCat(Dest: PWideChar; const Source: PWideChar): PWideChar;
@@ -2190,6 +2197,17 @@ end;
 // byte to go from LSB to MSB and vice versa.
 // EAX contains address of string
 procedure StrSwapByteOrder(Str: PWideChar);
+{$IFDEF SYN_COMPILER_16_UP}
+var
+  P: PWord;
+begin
+  P := PWord(Str);
+  while P^ <> 0 do 
+  begin
+    P^ := MakeWord(HiByte(P^), LoByte(P^));
+    Inc(P);
+  end;
+{$ELSE}
 asm
        PUSH    ESI
        PUSH    EDI
@@ -2208,6 +2226,7 @@ asm
 @@2:
        POP     EDI
        POP     ESI
+{$ENDIF}
 end;
 
 // works like QuotedStr from SysUtils.pas but can insert any quotation character
@@ -2571,7 +2590,7 @@ var
   ErrorMessage: UnicodeString;
 {$ENDIF}
 begin
-  if Mode = fmCreate then
+  if ((Mode and fmCreate) = fmCreate) then
   begin
     inherited Create(WideFileCreate(FileName, Rights));
     if Handle < 0 then
@@ -3150,7 +3169,7 @@ begin
           UnicodeStrings.Text := UTF8ToUnicodeString(UTF8Str);
 {$ELSE}
           UnicodeStrings.Text := UTF8Decode(UTF8Str);
-          UnicodeStrings.SaveUnicode := True;
+          UnicodeStrings.SaveFormat := sfUTF8;
 {$ENDIF}
         end;
       seUTF16LE:
@@ -3159,7 +3178,7 @@ begin
           Stream.ReadBuffer(WideStr[1], Size);
           UnicodeStrings.Text := WideStr;
 {$IFNDEF UNICODE}
-          UnicodeStrings.SaveUnicode := True;
+          UnicodeStrings.SaveFormat := sfUTF16LSB;
 {$ENDIF}
         end;
       seUTF16BE:
@@ -3169,7 +3188,7 @@ begin
           StrSwapByteOrder(PWideChar(WideStr));
           UnicodeStrings.Text := WideStr;
 {$IFNDEF UNICODE}
-          UnicodeStrings.SaveUnicode := True;
+          UnicodeStrings.SaveFormat := sfUTF16MSB;
 {$ENDIF}
         end;
       seAnsi:
@@ -3178,7 +3197,7 @@ begin
           Stream.ReadBuffer(AnsiStr[1], Size);
           UnicodeStrings.Text := UnicodeString(AnsiStr);
 {$IFNDEF UNICODE}
-          UnicodeStrings.SaveUnicode := False;
+          UnicodeStrings.SaveFormat := sfAnsi;
 {$ENDIF}
         end;
     end;

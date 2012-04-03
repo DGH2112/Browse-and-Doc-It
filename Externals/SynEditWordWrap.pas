02ebdf1562ff0189c8e5bde6b25480971dc712fb
DGH2112 <davidghoyle@gmail.com>
2012-04-03 16:19:54 +0100

    Updated SynEdit components to their latest version from the SVN repository.

--- Externals/SynEditWordWrap.pas
+++ Externals/SynEditWordWrap.pas
@@ -62,10 +62,13 @@ var
   // Accumulate/hide whitespace at EOL (at end of wrapped rows, actually)
   OldWhitespaceBehaviour: Boolean = False;
 
+const
+  MaxIndex = MaxInt div 16;
+
 type
-  TLineIndex = 0..MaxListSize;
-  TRowIndex = 0..MaxListSize;
-  TRowLength = byte;
+  TLineIndex = 0..MaxIndex;
+  TRowIndex = 0..MaxIndex;
+  TRowLength = word;
 
   TRowIndexArray = array [TLineIndex] of TRowIndex;
   PRowIndexArray = ^TRowIndexArray;
@@ -420,13 +423,15 @@ var
   vStartRow: Integer; // first row of the line
   vOldNextRow: Integer; // first row of the next line, before the change
   cLine: Integer;
+
+  p : PRowIndexArray;
 begin
   // ****** First parse the new string using an auxiliar array *****
   vLine := TSynEditStringList(Editor.Lines).ExpandedStrings[aIndex];
   vLine := Editor.ExpandAtWideGlyphs(vLine);
   // Pre-allocate a buffer for rowlengths
   vMaxNewRows := ((Length(vLine) - 1) div fMinRowLength) + 1;
-  vTempRowLengths := AllocMem(vMaxNewRows);
+  vTempRowLengths := AllocMem(vMaxNewRows * SizeOf(TRowLength));
   try
     vLineRowCount := 0;
     vRowBegin := PWideChar(vLine);
@@ -499,8 +504,19 @@ begin
       begin
         // ...if growing, update offsets (and thus RowCount) before rowlengths
         GrowRows(RowCount + Result);
-        for cLine := aIndex to LineCount - 1 do
-          Inc(fLineOffsets[cLine], Result);
+        if Result = 1 then begin
+          // EG: this makes Schlemiel run twice as fast, but doesn't solve
+          // the algorithmic issue if someone can spend some time looking
+          // at the big picture... there are huge speedups to be made by
+          // eliminating this loop
+          p:=fLineOffsets;
+          for cLine := aIndex to LineCount - 1 do
+             Inc(p[cLine])
+        end else begin
+          p:=fLineOffsets;
+          for cLine := aIndex to LineCount - 1 do
+            Inc(p[cLine], Result);
+        end;
         if vOldNextRow < RowCount - Result then
           MoveRows(vOldNextRow, Result);
       end

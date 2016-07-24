4c06f169d3ffbfbb1d0002a96545a22835b21039
DGH2112 <davidghoyle@gmail.com>
2016-07-24 18:23:59 +0100

    Fixed AV in BaseLanguageModule in the new TGenericParameter.IsEqual() method.

--- Source/BaseLanguageModule.pas
+++ Source/BaseLanguageModule.pas
@@ -3,7 +3,7 @@
   This module contains the base class for all language module to derived from
   and all standard constants across which all language modules have in common.
 
-  @Date    20 Jul 2016
+  @Date    24 Jul 2016
   @Version 1.0
   @Author  David Hoyle
 
@@ -4493,8 +4493,10 @@ end;
 Function TGenericParameter.IsEquals(Parameter : TGenericParameter) : Boolean;
 
 Begin
-  Result := (ParamModifier = Parameter.ParamModifier) And
-    (ParamType.AsString(False, False) = Parameter.ParamType.AsString(False, False));
+  Result := (ParamModifier = Parameter.ParamModifier);
+  If Result And (ParamType <> Nil) Then
+    Result := Result And
+      (ParamType.AsString(False, False) = Parameter.ParamType.AsString(False, False));
 End;
 
 (** --------------------------------------------------------------------------

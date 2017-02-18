821674971b1bfb41d0ffa029d2da7ecc7c5b36a0
DGH2112 <davidghoyle@gmail.com>
2017-02-18 13:15:56 +0000

    Refactored code into smaller modules (generally a single class per module) for better code maintenance. This class was formerly in TBaseLanguageModule (now BADI.Base.Module).\n

---
+++ Source/BADI.Generic.Variable.pas
@@ -0,0 +1,57 @@
+(**
+
+  This module contains a class to represent a generic variable.
+
+  @Author  David Hoyle
+  @Version 1.0
+  @Date    18 Feb 2017
+
+**)
+Unit BADI.Generic.Variable;
+
+Interface
+
+Uses
+  BADI.ElementContainer;
+
+{$INCLUDE CompilerDefinitions.inc}
+
+Type
+  (** This is a sub class for all variables **)
+  TGenericVariable = Class {$IFDEF D2005} Abstract {$ENDIF} (TElementContainer)
+  Public
+    Procedure CheckDocumentation(Var boolCascade: Boolean); Override;
+  End;
+
+Implementation
+
+uses
+  BADI.Types,
+  BADI.Options,
+  BADI.ResourceStrings,
+  BADI.Constants;
+
+(**
+
+  This method checks the documentation for the given clause item.
+
+  @precon  C is a valid generic container to be checked for clause like
+           documentation.
+  @postcon Checks the passed clause for documentation errors.
+
+  @param   boolCascade as a Boolean as a reference
+
+**)
+Procedure TGenericVariable.CheckDocumentation(var boolCascade : Boolean);
+
+Begin
+  If doShowUndocumentedVars In BrowseAndDocItOptions.Options Then
+    If ((Comment = Nil) Or (Comment.TokenCount = 0)) And (Scope <> scLocal) Then
+      Begin
+        AddDocumentConflict([Identifier], Line, Column, Comment,
+          strVariableDocumentation, DocConflictTable[dctVariableClauseUndocumented]);
+      End;
+  Inherited CheckDocumentation(boolCascade);
+End;
+
+End.

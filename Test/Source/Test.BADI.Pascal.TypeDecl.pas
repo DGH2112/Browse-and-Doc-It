ec9f4e534a74a4baae786e5673e1989977142e8e
DGH2112 <davidghoyle@gmail.com>
2017-02-24 19:49:37 +0000

    Refactored code into smaller modules (generally a single class per module) for better code maintenance. This class was formerly in TestPascalModule (now Test.BADI.Pascal.Module).

---
+++ Test/Source/Test.BADI.Pascal.TypeDecl.pas
@@ -0,0 +1,60 @@
+Unit Test.BADI.Pascal.TypeDecl;
+
+Interface
+
+Uses
+  TestFramework,
+  Test.BADI.Base.Module,
+  BADI.Pascal.TypeDecl;
+
+Type
+  TestTTypes = Class(TExtendedTestCase)
+  Strict Private
+    FTypes: TTypes;
+  Public
+    Procedure SetUp; Override;
+    Procedure TearDown; Override;
+  Published
+    Procedure TestCreate;
+    Procedure TestAsString;
+  End;
+
+Implementation
+
+Uses
+  BADI.Types;
+
+Procedure TestTTypes.SetUp;
+
+Begin
+  FTypes := TTypes.Create('MyType', scPrivate, 12, 23, iiPublicType, Nil);
+End;
+
+Procedure TestTTypes.TearDown;
+
+Begin
+  FTypes.Free;
+  FTypes := Nil;
+End;
+
+Procedure TestTTypes.TestAsString;
+
+Begin
+  CheckEquals('MyType', FTypes.AsString(True, False));
+  FTypes.AddToken('Integer');
+  CheckEquals('MyType = Integer', FTypes.AsString(True, False));
+End;
+
+Procedure TestTTypes.TestCreate;
+
+Begin
+  CheckEquals(scPrivate, FTypes.Scope);
+  CheckEquals(12, FTypes.Line);
+  CheckEquals(23, FTypes.Column);
+  CheckEquals(iiPublicType, FTypes.ImageIndex);
+  CheckEquals(iiPrivateType, FTypes.ImageIndexAdjustedForScope);
+End;
+
+Initialization
+  RegisterTest('PascalModule Tests', TestTTypes.Suite);
+End.

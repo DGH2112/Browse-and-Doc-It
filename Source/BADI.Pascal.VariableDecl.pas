5325b42f149803e17b27250dfe974553cffa82fb
DGH2112 <davidghoyle@gmail.com>
2017-02-24 19:58:37 +0000

    Refactored code into smaller modules (generally a single class per module) for better code maintenance. This class was formerly in PascalModule (now BADI.Pascal.Module).

---
+++ Source/BADI.Pascal.VariableDecl.pas
@@ -0,0 +1,52 @@
+(**
+
+  This module contains a class which implements an Object Pascal specific variable Declaration.
+
+  @Author  David Hoyle
+  @Version 1.0
+  @Date    24 Feb 2017
+
+**)
+Unit BADI.Pascal.VariableDecl;
+
+Interface
+
+Uses
+  BADI.Generic.Variable;
+
+Type
+  (** This is a sub class for all variables. **)
+  TVar = Class(TGenericVariable)
+    {$IFDEF D2005} Strict {$ENDIF} Protected
+  Public
+    Function AsString(boolShowIdentifier, boolForDocumentation: Boolean): String; Override;
+  End;
+
+Implementation
+
+Uses
+  BADI.Types,
+  BADI.Options,
+  BADI.ResourceStrings,
+  BADI.Constants;
+
+(**
+
+  This is a getter method for the AsString property.
+
+  @precon  None.
+  @postcon Returns the variable declaration.
+
+  @param   boolShowIdentifier   as a Boolean
+  @param   boolForDocumentation as a Boolean
+  @return  a String
+
+**)
+Function TVar.AsString(boolShowIdentifier, boolForDocumentation: Boolean): String;
+
+Begin
+  Result := BuildStringRepresentation(boolShowIdentifier, boolForDocumentation, ':',
+    BrowseAndDocItOptions.MaxDocOutputWidth);
+End;
+
+End.

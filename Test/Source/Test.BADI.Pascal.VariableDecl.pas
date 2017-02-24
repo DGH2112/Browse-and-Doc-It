5ad1030615aec0112f5d4c5dd621fc8f4a55ffba
DGH2112 <davidghoyle@gmail.com>
2017-02-24 19:49:07 +0000

    Refactored code into smaller modules (generally a single class per module) for better code maintenance. This class was formerly in TestPascalModule (now Test.BADI.Pascal.Module).

---
+++ Test/Source/Test.BADI.Pascal.VariableDecl.pas
@@ -0,0 +1,56 @@
+Unit Test.BADI.Pascal.VariableDecl;
+
+Interface
+
+Uses
+  TestFramework,
+  Test.BADI.Base.Module,
+  BADI.Pascal.VariableDecl;
+
+Type
+  TestTVar = Class(TExtendedTestCase)
+  Strict Private
+    FVar: TVar;
+  Public
+    Procedure SetUp; Override;
+    Procedure TearDown; Override;
+  Published
+    Procedure TestCreate;
+    Procedure TestAsString;
+  End;
+
+Implementation
+
+Uses
+  BADI.Types;
+
+Procedure TestTVar.SetUp;
+Begin
+  FVar := TVar.Create('MyVar', scPrivate, 12, 23, iiPublicVariable, Nil);
+  FVar.AddToken('Integer');
+End;
+
+Procedure TestTVar.TearDown;
+Begin
+  FVar.Free;
+  FVar := Nil;
+End;
+
+Procedure TestTVar.TestAsString;
+Begin
+  Checkequals('MyVar : Integer', FVar.AsString(True, False));
+End;
+
+Procedure TestTVar.TestCreate;
+Begin
+  Checkequals('MyVar', FVar.Identifier);
+  Checkequals(scPrivate, FVar.Scope);
+  Checkequals(12, FVar.Line);
+  Checkequals(23, FVar.Column);
+  Checkequals(iiPublicVariable, FVar.ImageIndex);
+  Checkequals(iiPrivateVariable, FVar.ImageIndexAdjustedForScope);
+End;
+
+Initialization
+  RegisterTest('PascalModule Tests', TestTVar.Suite);
+End.

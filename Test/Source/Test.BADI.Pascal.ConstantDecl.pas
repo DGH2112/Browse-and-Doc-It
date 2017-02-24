5a641fa3864a7c5f11f4c56f464a3a055c77562e
DGH2112 <davidghoyle@gmail.com>
2017-02-24 19:53:31 +0000

    Refactored code into smaller modules (generally a single class per module) for better code maintenance. This class was formerly in TestPascalModule (now Test.BADI.Pascal.Module).

---
+++ Test/Source/Test.BADI.Pascal.ConstantDecl.pas
@@ -0,0 +1,61 @@
+Unit Test.BADI.Pascal.ConstantDecl;
+
+Interface
+
+Uses
+  TestFramework,
+  Test.BADI.Base.Module,
+  BADI.Pascal.ConstantDecl;
+
+Type
+  TestTConstant = Class(TExtendedTestCase)
+  Strict Private
+    FConstant: TConstant;
+  Public
+    Procedure SetUp; Override;
+    Procedure TearDown; Override;
+  Published
+    Procedure TestCreate;
+    Procedure TestAsString;
+  End;
+
+Implementation
+
+Uses
+  BADI.Types;
+
+Procedure TestTConstant.SetUp;
+Begin
+  FConstant := TConstant.Create('MyConstant', scPrivate, 12, 23, iiPublicConstant, Nil);
+  FConstant.AddToken('1');
+End;
+
+Procedure TestTConstant.TearDown;
+Begin
+  FConstant.Free;
+  FConstant := Nil;
+End;
+
+Procedure TestTConstant.TestAsString;
+Begin
+  Checkequals('MyConstant = 1', FConstant.AsString(True, False));
+  FConstant.AddToken('+');
+  FConstant.AddToken('2');
+  FConstant.AddToken('*');
+  FConstant.AddToken('3');
+  Checkequals('MyConstant = 1 + 2 * 3', FConstant.AsString(True, False));
+End;
+
+Procedure TestTConstant.TestCreate;
+Begin
+  Checkequals('MyConstant', FConstant.Identifier);
+  Checkequals(scPrivate, FConstant.Scope);
+  Checkequals(12, FConstant.Line);
+  Checkequals(23, FConstant.Column);
+  Checkequals(iiPublicConstant, FConstant.ImageIndex);
+  Checkequals(iiPrivateConstant, FConstant.ImageIndexAdjustedForScope);
+End;
+
+Initialization
+  RegisterTest('PascalModule Tests', TestTConstant.Suite);
+End.

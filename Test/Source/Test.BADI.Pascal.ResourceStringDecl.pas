2c1214927a8c4e897ddbd89b81adf5b0fc12409b
DGH2112 <davidghoyle@gmail.com>
2017-02-24 19:54:09 +0000

    Refactored code into smaller modules (generally a single class per module) for better code maintenance. This class was formerly in TestPascalModule (now Test.BADI.Pascal.Module).

---
+++ Test/Source/Test.BADI.Pascal.ResourceStringDecl.pas
@@ -0,0 +1,73 @@
+Unit Test.BADI.Pascal.ResourceStringDecl;
+
+Interface
+
+Uses
+  TestFramework,
+  Test.BADI.Base.Module,
+  BADI.Pascal.ResourceStringDecl;
+
+Type
+  TestTResourceString = Class(TExtendedTestCase)
+  Strict Private
+    FResourceString: TResourceString;
+  Public
+    Procedure SetUp; Override;
+    Procedure TearDown; Override;
+  Published
+    Procedure TestCreate;
+    Procedure TestCheckDocumentation;
+  End;
+
+Implementation
+
+Uses
+  BADI.Types, BADI.Comment;
+
+Procedure TestTResourceString.SetUp;
+Begin
+  FResourceString := TResourceString.Create('MyResourceString', scPublic, 12, 23,
+    iiPublicResourceString, Nil);
+  FResourceString.AddToken('''This is a string literal.''');
+End;
+
+Procedure TestTResourceString.TearDown;
+Begin
+  FResourceString.Free;
+  FResourceString := Nil;
+End;
+
+Procedure TestTResourceString.TestCheckDocumentation;
+Var
+  boolCascade: Boolean;
+  C: TComment;
+Begin
+  CheckEquals(0, FResourceString.ElementCount);
+  FResourceString.CheckDocumentation(boolCascade);
+  CheckEquals(1, FResourceString.ElementCount);
+  CheckEquals('1) Resource string ''MyResourceString'' is undocumented.',
+    FResourceString.DocConflict(1));
+  FResourceString.DeleteDocumentConflicts;
+  C := TComment.Create('This is a comment for the resource string.', 0, 0);
+  Try
+    FResourceString.Comment := C;
+    FResourceString.CheckDocumentation(boolCascade);
+    CheckEquals(0, FResourceString.ElementCount);
+  Finally
+    C.Free;
+  End;
+End;
+
+Procedure TestTResourceString.TestCreate;
+Begin
+  CheckEquals('MyResourceString', FResourceString.Identifier);
+  CheckEquals(scPublic, FResourceString.Scope);
+  CheckEquals(12, FResourceString.Line);
+  CheckEquals(23, FResourceString.Column);
+  CheckEquals(iiPublicResourceString, FResourceString.ImageIndex);
+  CheckEquals(iiPublicResourceString, FResourceString.ImageIndexAdjustedForScope);
+End;
+
+Initialization
+  RegisterTest('PascalModule Tests', TestTResourceString.Suite);
+End.

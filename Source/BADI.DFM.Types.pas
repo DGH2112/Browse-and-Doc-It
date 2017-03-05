dbdc1b18bcfca1bf2afb28eb0bbc77926b4f9953
DGH2112 <davidghoyle@gmail.com>
2017-03-05 20:06:53 +0000

    Refactored code into smaller modules (generally a single class per module) for better code maintenance. These types was formerly in DFMModule (now BADI.DFM.Module).

---
+++ Source/BADI.DFM.Types.pas
@@ -0,0 +1,20 @@
+(**
+
+  This module contains types for the DFM parser.
+
+  @Author  David Hoyle
+  @Version 1.0
+  @Date    05 Mar 2017
+
+**)
+Unit BADI.DFM.Types;
+
+Interface
+
+Type
+  (** An enumerate to define the type of an object definition. **)
+  TObjectType = (otObject, otInherited, otinline);
+
+Implementation
+
+End.

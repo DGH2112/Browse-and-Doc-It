3c9e6a980c8b877d0fef3d5cb5320b51f85fe015
DGH2112 <davidghoyle@gmail.com>
2017-03-11 19:37:03 +0000

    Refactored code into smaller modules (generally a single class per module) for better code maintenance. This class was formerly in XMLModule (now .BADI.XML.Module).

---
+++ Source/BADI.XML.XMLIgnoreElement.pas
@@ -0,0 +1,47 @@
+(**
+
+  This modlue contains a class to represent an XML Ignore element.
+
+  @Author  David Hoyle
+  @Version 1.0
+  @Date    11 Mar 2017
+
+**)
+Unit BADI.XML.XMLIgnoreElement;
+
+Interface
+
+Uses
+  BADI.XML.BaseElement;
+
+Type
+  (** This class represents the individual xml PERef declarations in the document. **)
+  TXMLIgnoreElement = Class(TXMLBaseElement)
+  Public
+    Function AsString(boolShowIdenifier, boolForDocumentation: Boolean): String; Override;
+  End;
+
+Implementation
+
+Uses
+  BADI.Options;
+
+(**
+
+  This method returns a string presentation of the XML PE Ref element.
+
+  @precon  None.
+  @postcon Returns a string presentation of the XML PE Ref element.
+
+  @param   boolShowIdenifier    as a Boolean
+  @param   boolForDocumentation as a Boolean
+  @return  a String
+
+**)
+Function TXMLIgnoreElement.AsString(boolShowIdenifier, boolForDocumentation: Boolean): String;
+Begin
+  Result := BuildStringRepresentation(True, False, '', BrowseAndDocItOptions.MaxDocOutputWidth,
+    [']'], ['[']);
+End;
+
+End.

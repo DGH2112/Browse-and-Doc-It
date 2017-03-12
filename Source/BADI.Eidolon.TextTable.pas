8fdf1578bc9fdd53be61e24ef07da0bc7312efd7
DGH2112 <davidghoyle@gmail.com>
2017-03-12 11:49:53 +0000

    Refactored code into smaller modules (generally a single class per module) for better code maintenance. This class was formerly in EidolonModule (now .BADI.Eidolon..Module).

---
+++ Source/BADI.Eidolon.TextTable.pas
@@ -0,0 +1,57 @@
+(**
+
+  This module contains a class to represent an Eidolon Text Table.
+
+  @Author  David Hoyle
+  @Version 1.0
+  @Date    12 Mar 2017
+
+**)
+Unit BADI.Eidolon.TextTable;
+
+Interface
+
+{$INCLUDE CompilerDefinitions.inc}
+
+Uses
+  BADI.Eidolon.BaseTable;
+
+Type
+  (** A class to represent a TextTable definition. **)
+  TTextTable = Class(TBaseTable)
+    FFileName : String;
+  Public
+    Function AsString(boolShowIdentifier, boolForDocumentation : Boolean) : String; Override;
+    (**
+      This method gets and sets the filename of the text table.
+      @precon  None.
+      @postcon Gets or sets the filename of the text table.
+      @return  a String
+    **)
+    Property FileName : String Read FFileName Write FFileName;
+  End;
+
+Implementation
+
+{ TTextTable }
+
+(**
+
+  This method returns string representation of the text table definition.
+
+  @precon  None.
+  @postcon Returns string representation of the text table definition.
+
+  @param   boolShowIdentifier   as a Boolean
+  @param   boolForDocumentation as a Boolean
+  @return  a String
+
+**)
+function TTextTable.AsString(boolShowIdentifier,
+  boolForDocumentation: Boolean): String;
+
+begin
+  Result := Identifier + '=Class(TextTable)';
+end;
+
+End.

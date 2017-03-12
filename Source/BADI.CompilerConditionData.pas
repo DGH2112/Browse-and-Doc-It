9b6fbedaf4b20d5751e15333510cec6fcd7ee642
DGH2112 <davidghoyle@gmail.com>
2017-03-12 13:50:20 +0000

    Added the compiler definition type to the compiler stack.

--- Source/BADI.CompilerConditionData.pas
+++ Source/BADI.CompilerConditionData.pas
@@ -4,7 +4,7 @@
 
   @Author  David Hoyle
   @Version 1.0
-  @Date    18 Feb 2017
+  @Date    12 Mar 2017
 
 **)
 Unit BADI.CompilerConditionData;
@@ -18,25 +18,34 @@ Type
   (** This class represents a single stack entry on the CompilerCondition stacks. **)
   TCompilerConditionData = Class
     {$IFDEF D2005} Strict {$ENDIF} Private
+    FCompilerDefType : TCompilerDefType;
     FCompilerCondition: TCompilerCondition;
     FTokenIndex: TTokenIndex;
     {$IFDEF D2005} Strict {$ENDIF} Protected
   Public
-    Constructor Create(iCompilerCondition: TCompilerCondition; iTokenIndex: TTokenIndex);
+    Constructor Create(Const iCompilerDefType : TCompilerDefType;
+      Const iCompilerCondition: TCompilerCondition; Const iTokenIndex: TTokenIndex);
+    (**
+      This property provide access to the compiler definition type of the class.
+      @precon  None.
+      @postcon Returns the compiler definiton type of the stack item.
+      @return  a TCompilerDefType
+    **)
+    Property CompilerDefType : TCompilerDefType Read FCompilerDefType;
     (**
       This property gets and sets the compiler condition (include code or exclude code).
       @precon  None.
       @postcon Gets and sets the compiler condition (include code or exclude code).
       @return  a TCompilerCondition
     **)
-    Property CompilerCondition: TCompilerCondition Read FCompilerCondition Write FCompilerCondition;
+    Property CompilerCondition: TCompilerCondition Read FCompilerCondition;
     (**
       This property gets or sets the token index of the compiler condition.
       @precon  None.
       @postcon Gets or sets the token index of the compiler condition.
       @return  a TTokenIndex
     **)
-    Property TokenIndex: TTokenIndex Read FTokenIndex Write FTokenIndex;
+    Property TokenIndex: TTokenIndex Read FTokenIndex;
   End;
 
 Implementation
@@ -48,14 +57,16 @@ Implementation
   @precon  None.
   @postcon Initialises the class with data.
 
-  @param   iCompilerCondition as a TCompilerCondition
-  @param   iTokenIndex        as a TTokenIndex
+  @param   iCompilerDefType   as a TCompilerDefType as a constant
+  @param   iCompilerCondition as a TCompilerCondition as a constant
+  @param   iTokenIndex        as a TTokenIndex as a constant
 
 **)
-Constructor TCompilerConditionData.Create(iCompilerCondition: TCompilerCondition;
-  iTokenIndex: TTokenIndex);
+Constructor TCompilerConditionData.Create(Const iCompilerDefType : TCompilerDefType;
+  Const iCompilerCondition: TCompilerCondition; Const iTokenIndex: TTokenIndex);
 
 Begin
+  FCompilerDefType := iCompilerDefType;
   FCompilerCondition := iCompilerCondition;
   FTokenIndex := iTokenIndex;
 End;

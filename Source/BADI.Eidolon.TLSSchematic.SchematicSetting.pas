a823f1bd841d2932630abd28cfebf2737d35ccc8
DGH2112 <davidghoyle@gmail.com>
2017-03-12 09:50:38 +0000

    Refactored code into smaller modules (generally a single class per module) for better code maintenance. This class was formerly in EidolonTLSSchematicModule (now .BADI.Eidolon.TLSSchematic.Module).

---
+++ Source/BADI.Eidolon.TLSSchematic.SchematicSetting.pas
@@ -0,0 +1,59 @@
+(**
+
+  This module contains a class to represent a schematic setting.
+
+  @Author  David Hoyle
+  @Version 1.0
+  @Date    06 Mar 2017
+
+**)
+Unit BADI.Eidolon.TLSSchematic.SchematicSetting;
+
+Interface
+
+Uses
+  BADI.ElementContainer;
+
+{$INCLUDE CompilerDefinitions.inc}
+
+Type
+  (** A class to represent a schematic setting. **)
+  TSchematicSetting = Class(TElementContainer)
+  {$IFDEF D2005} Strict {$ENDIF} Private
+    FPercentage : Double;
+  {$IFDEF D2005} Strict {$ENDIF} Protected
+  Public
+    Function AsString(boolShowIdentifier, boolForDocumentation : Boolean) : String; Override;
+    (**
+      This property gets ans sets the percentage of the settings.
+      @precon  None.
+      @postcon Gets ans sets the percentage of the settings.
+      @return  an Double
+    **)
+    Property Percentage : Double Read FPercentage Write FPercentage;
+  End;
+
+Implementation
+
+Uses
+  SysUtils;
+
+(**
+
+  This method returns a string representation of a schematic setting.
+
+  @precon  None.
+  @postcon Returns a string representation of a schematic setting.
+
+  @param   boolShowIdentifier   as a Boolean
+  @param   boolForDocumentation as a Boolean
+  @return  a String
+
+**)
+function TSchematicSetting.AsString(boolShowIdentifier, boolForDocumentation: Boolean): String;
+
+begin
+  Result := Format('%s %1.1f%%', [Identifier, FPercentage]);
+end;
+
+End.

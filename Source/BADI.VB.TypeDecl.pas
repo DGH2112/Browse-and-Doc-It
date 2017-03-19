5b34ec189d189a8ab53c3b4bd46310a58bd56eaf
DGH2112 <davidghoyle@gmail.com>
2017-03-19 10:32:14 +0000

    Refactored code into smaller modules (generally a single class per module) for better code maintenance. This class was formerly in VBModule (now BADI.VB.Module).

---
+++ Source/BADI.VB.TypeDecl.pas
@@ -0,0 +1,51 @@
+(**
+
+  This module contains a class to represent a general type declaration.
+
+  @Author  David Hoyle
+  @Version 1.0
+  @Date    19 Mar 2017
+
+**)
+Unit BADI.VB.TypeDecl;
+
+Interface
+
+{$INCLUDE CompilerDefinitions.Inc}
+
+Uses
+  BADI.Generic.TypeDecl;
+
+Type
+  (** A class to represent types in visual basic. **)
+  TVBTypeDecl = Class(TGenericTypeDecl)
+  {$IFDEF D2005} Strict {$ENDIF} Protected
+  Public
+    Function AsString(boolShowIdentifier, boolForDocumentation : Boolean) : String; Override;
+  End;
+
+Implementation
+
+Uses
+  BADI.Options;
+
+(**
+
+  This method returns a string representation of the visual basic return type.
+
+  @precon  None .
+  @postcon Returns a string representation of the visual basic return type .
+
+  @param   boolShowIdentifier   as a Boolean
+  @param   boolForDocumentation as a Boolean
+  @return  a String
+
+**)
+Function TVBTypeDecl.AsString(boolShowIdentifier, boolForDocumentation: Boolean): String;
+Begin
+  Result := BuildStringRepresentation(boolShowIdentifier, boolForDocumentation, '',
+    BrowseAndDocItOptions.MaxDocOutputWidth);
+End;
+
+
+End.

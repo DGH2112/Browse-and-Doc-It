df99654d522860083292a6fc4ed84c719046935f
DGH2112 <davidghoyle@gmail.com>
2017-03-19 10:35:50 +0000

    Refactored code into smaller modules (generally a single class per module) for better code maintenance. This class was formerly in VBModule (now BADI.VB.Module).

---
+++ Source/BADI.VB.Version.pas
@@ -0,0 +1,55 @@
+(**
+
+  This module contains a class to represent a VB Version declaration.
+
+  @Author  David Hoyle
+  @Version 1.0
+  @Date    19 Mar 2017
+
+**)
+Unit BADI.VB.Version;
+
+Interface
+
+{$INCLUDE CompilerDefinitions.Inc}
+
+Uses
+  BADI.ElementContainer;
+
+Type
+  (** A class to represent versions **)
+  TVBVersion = Class(TElementContainer)
+  {$IFDEF D2005} Strict {$ENDIF} Protected
+  Public
+    Function AsString(boolShowIdentifier, boolForDocumentation : Boolean) : String; Override;
+  End;
+
+Implementation
+
+Uses
+  BADI.Options;
+
+(**
+
+  This method returns a string representation of the visual basic version.
+
+  @precon  None .
+  @postcon Returns a string representation of the visual basic version .
+
+  @param   boolShowIdentifier   as a Boolean
+  @param   boolForDocumentation as a Boolean
+  @return  a String
+
+**)
+Function TVBVersion.AsString(boolShowIdentifier, boolForDocumentation: Boolean): String;
+Begin
+  Result := BuildStringRepresentation(False, boolForDocumentation,
+    '', BrowseAndDocItOptions.MaxDocOutputWidth);
+  If boolShowIdentifier Then
+    If Result <> '' Then
+      Result := Identifier + #32 + Result
+    Else
+      Result := Identifier;
+End;
+
+End.

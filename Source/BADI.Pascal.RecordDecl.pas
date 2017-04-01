0683028f5a2750b9d0cabeefbdbe6000e27c7f9c
DGH2112 <davidghoyle@gmail.com>
2017-04-01 12:35:08 +0100

    Moved the registration of the module to the new BADI.Initialisation module / class and make the BADI Options and BADI Module Dispatcher singleton class using a class metod to access their instances. BADI.Initialisation now managews these classes.

--- Source/BADI.Pascal.RecordDecl.pas
+++ Source/BADI.Pascal.RecordDecl.pas
@@ -4,7 +4,7 @@
 
   @Author  David Hoyle
   @Version 1.0
-  @Date    24 Feb 2017
+  @Date    01 Apr 2017
 
 **)
 Unit BADI.Pascal.RecordDecl;
@@ -180,7 +180,7 @@ var
   i: Integer;
 
 Begin
-  If doShowUndocumentedRecords In BrowseAndDocItOptions.Options Then
+  If doShowUndocumentedRecords In BADIOptions.Options Then
     If ((Comment = Nil) Or (Comment.TokenCount = 0)) And (Scope <> scLocal) Then
       AddDocumentConflict([Identifier], Line, Column, Comment,
         strRecordDocumentation, DocConflictTable[dctRecordClauseUndocumented]);

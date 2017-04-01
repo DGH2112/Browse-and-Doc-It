4a83c7c6477a82e3f184cf4d5ccae25fe9526121
DGH2112 <davidghoyle@gmail.com>
2017-04-01 12:34:50 +0100

    Moved the registration of the module to the new BADI.Initialisation module / class and make the BADI Options and BADI Module Dispatcher singleton class using a class metod to access their instances. BADI.Initialisation now managews these classes.

--- Source/BADI.Pascal.ObjectDecl.pas
+++ Source/BADI.Pascal.ObjectDecl.pas
@@ -4,7 +4,7 @@
 
   @Author  David Hoyle
   @Version 1.0
-  @Date    24 Feb 2017
+  @Date    01 Apr 2017
 
 **)
 Unit BADI.Pascal.ObjectDecl;
@@ -85,7 +85,7 @@ var
   i: Integer;
 
 Begin
-  If doShowUndocumentedObjects In BrowseAndDocItOptions.Options Then
+  If doShowUndocumentedObjects In BADIOptions.Options Then
     If ((Comment = Nil) Or (Comment.TokenCount = 0)) And (Scope <> scLocal) Then
       AddDocumentConflict([Identifier], Line, Column, Comment,
         strObjectDocumentation, DocConflictTable[dctObjectClauseUndocumented]);

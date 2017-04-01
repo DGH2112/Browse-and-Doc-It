ecc183a90aabc3e4099b4b9d5ea29d42513c66a5
DGH2112 <davidghoyle@gmail.com>
2017-04-01 12:23:36 +0100

    Moved the registration of the module to the new BADI.Initialisation module / class and make the BADI Options and BADI Module Dispatcher singleton class using a class metod to access their instances. BADI.Initialisation now managews these classes.

--- Source/BADI.XML.XMLPI.pas
+++ Source/BADI.XML.XMLPI.pas
@@ -4,7 +4,7 @@
 
   @Author  David Hoyle
   @Version 1.0
-  @Date    11 Mar 2017
+  @Date    01 Apr 2017
 
 **)
 Unit BADI.XML.XMLPI;
@@ -42,7 +42,7 @@ Uses
 Function TXMLPI.AsString(boolShowIdenifier, boolForDocumentation: Boolean): String;
 Begin
   Result := BuildStringRepresentation(boolShowIdenifier, boolForDocumentation, '',
-    BrowseAndDocItOptions.MaxDocOutputWidth, [#32, '=', '?'], [#32, '=', '?'], []);
+    BADIOptions.MaxDocOutputWidth, [#32, '=', '?'], [#32, '=', '?'], []);
 End;
 
 End.

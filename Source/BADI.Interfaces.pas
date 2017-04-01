6a8fa46d01ebcbcddab01d9505a96e1755baceb7
DGH2112 <davidghoyle@gmail.com>
2017-04-01 12:39:10 +0100

    Initial import of the interfaces modules (empty) in preparation of implementing interfaces in BADI.

---
+++ Source/BADI.Interfaces.pas
@@ -0,0 +1,16 @@
+(**
+
+  This module contains interfaces for use throughout Browse and Doc It.
+
+  @Author  David Hoyle
+  @Version 1.0
+  @Date    31 Mar 2017
+
+**)
+Unit BADI.Interfaces;
+
+Interface
+
+Implementation
+
+End.

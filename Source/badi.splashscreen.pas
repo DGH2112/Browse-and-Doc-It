03d7a00c7424d7f362ef2d7028692db59ff6b2a0
DGH2112 <davidghoyle@gmail.com>
2017-04-11 11:27:40 +0100

    Removed the dependency on BADI.Common.

--- Source/badi.splashscreen.pas
+++ Source/badi.splashscreen.pas
@@ -4,7 +4,7 @@
 
   @Author  David Hoyle
   @Version 1.0
-  @Date    19 Feb 2017
+  @Date    11 Apr 2017
 
 **)
 Unit BADI.SplashScreen;
@@ -22,7 +22,8 @@ Uses
   SysUtils,
   Windows,
   Forms,
-  BADI.Common;
+  BADI.Functions,
+  BADI.Constants;
 
 (**
 

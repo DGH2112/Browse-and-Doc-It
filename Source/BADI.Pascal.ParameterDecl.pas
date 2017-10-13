b41433d24020c3768d218d9c066e60ac51958a53
DGH2112 <davidghoyle@gmail.com>
2017-10-13 21:36:37 +0100

    Updated the signatures with CONST parameters.

--- Source/BADI.Pascal.ParameterDecl.pas
+++ Source/BADI.Pascal.ParameterDecl.pas
@@ -4,7 +4,7 @@
 
   @Author  David Hoyle
   @Version 1.0
-  @Date    24 Feb 2017
+  @Date    13 Oct 2017
 
 **)
 Unit BADI.Pascal.ParameterDecl;
@@ -19,7 +19,7 @@ Type
   TPascalParameter = Class(TGenericParameter)
   {$IFDEF D2005} Strict {$ENDIF} Protected
   Public
-    Function AsString(boolShowIdentifier, boolForDocumentation : Boolean) : String; Override;
+    Function AsString(Const boolShowIdentifier, boolForDocumentation : Boolean) : String; Override;
   End;
 
 Implementation
@@ -32,16 +32,18 @@ uses
   This is a getter method for the AsString property.
 
   @precon  None.
-  @postcon Outputs the parameter information in the style of object pascal
-           code.
+  @postcon Outputs the parameter information in the style of object pascal code.
 
-  @param   boolShowIdentifier   as a Boolean
-  @param   boolForDocumentation as a Boolean
+  @param   boolShowIdentifier   as a Boolean as a constant
+  @param   boolForDocumentation as a Boolean as a constant
   @return  a String
 
 **)
-Function TPascalParameter.AsString(boolShowIdentifier,
-  boolForDocumentation : Boolean) : String;
+Function TPascalParameter.AsString(Const boolShowIdentifier, boolForDocumentation : Boolean) : String;
+
+Const
+  strEquals = #32'='#32;
+  strColon = #32':'#32;
 
 begin
   Result := '';
@@ -50,12 +52,12 @@ begin
   If ParamType <> Nil Then
     Begin
       If boolShowIdentifier Then
-        Result := Result + #32':'#32;
+        Result := Result + strColon;
       Result := Result + strArrayOf[ArrayOf];
       Result := Result + ParamType.AsString(False, boolForDocumentation);
     End;
   If DefaultValue <> '' Then
-    Result := Result + #32'='#32 + DefaultValue;
+    Result := Result + strEquals + DefaultValue;
 end;
 
 End.

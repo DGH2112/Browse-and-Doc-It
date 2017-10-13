2d371805bc2da6e6fd19b41852e48869098a95b8
DGH2112 <davidghoyle@gmail.com>
2017-10-13 21:34:42 +0100

    Updated the signatures with CONST parameters.

--- Source/BADI.Pascal.ExportsItem.pas
+++ Source/BADI.Pascal.ExportsItem.pas
@@ -4,7 +4,7 @@
 
   @Author  David Hoyle
   @Version 1.0
-  @Date    24 Feb 2017
+  @Date    12 Oct 2017
 
 **)
 Unit BADI.Pascal.ExportsItem;
@@ -23,9 +23,9 @@ Type
     FResolved: Boolean;
   {$IFDEF D2005} Strict {$ENDIF} Protected
   Public
-    Constructor Create(Const strName: String; AScope: TScope; iLine, iColumn: Integer;
-      AImageIndex: TBADIImageIndex; AComment: TComment); Override;
-    Function AsString(boolShowIdentifier, boolForDocumentation: Boolean): String; Override;
+    Constructor Create(Const strName: String; Const AScope: TScope; Const iLine, iColumn: Integer;
+      Const AImageIndex: TBADIImageIndex; Const AComment: TComment); Override;
+    Function AsString(Const boolShowIdentifier, boolForDocumentation: Boolean): String; Override;
     (**
       This property gets and sets whether the symbol is resolved.
       @precon  None.
@@ -44,12 +44,12 @@ Implementation
   @precon  None .
   @postcon Returns the Exported item declaration .
 
-  @param   boolShowIdentifier   as a Boolean
-  @param   boolForDocumentation as a Boolean
+  @param   boolShowIdentifier   as a Boolean as a constant
+  @param   boolForDocumentation as a Boolean as a constant
   @return  a String
 
 **)
-Function TExportsItem.AsString(boolShowIdentifier, boolForDocumentation: Boolean): String;
+Function TExportsItem.AsString(Const boolShowIdentifier, boolForDocumentation: Boolean): String;
 
 Var
   iToken: Integer;
@@ -74,15 +74,15 @@ End;
   @postcon Initialises the class.
 
   @param   strName     as a String as a constant
-  @param   AScope      as a TScope
-  @param   iLine       as an Integer
-  @param   iColumn     as an Integer
-  @param   AImageIndex as a TBADIImageIndex
-  @param   AComment    as a TComment
+  @param   AScope      as a TScope as a constant
+  @param   iLine       as an Integer as a constant
+  @param   iColumn     as an Integer as a constant
+  @param   AImageIndex as a TBADIImageIndex as a constant
+  @param   AComment    as a TComment as a constant
 
 **)
-Constructor TExportsItem.Create(Const strName: String; AScope: TScope; iLine, iColumn: Integer;
-  AImageIndex: TBADIImageIndex; AComment: TComment);
+Constructor TExportsItem.Create(Const strName: String; Const AScope: TScope; Const iLine, iColumn: Integer;
+      Const AImageIndex: TBADIImageIndex; Const AComment: TComment);
 
 Begin
   Inherited Create(strName, AScope, iLine, iColumn, AImageIndex, AComment);

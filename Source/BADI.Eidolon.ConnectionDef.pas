3c4a3ba81042d7356c96493ce45cbad25f78e519
DGH2112 <davidghoyle@gmail.com>
2017-10-13 21:48:31 +0100

    Updated the signatures with CONST parameters.

--- Source/BADI.Eidolon.ConnectionDef.pas
+++ Source/BADI.Eidolon.ConnectionDef.pas
@@ -4,7 +4,7 @@
 
   @Author  David Hoyle
   @Version 1.0
-  @Date    12 Mar 2017
+  @Date    12 Oct 2017
 
 **)
 Unit BADI.Eidolon.ConnectionDef;
@@ -21,7 +21,7 @@ Type
   (** A class to represent a connection definition. **)
   TConnectionDef = Class(TDBConnection)
   Public
-    Function AsString(boolShowIdentifier, boolForDocumentation : Boolean) : String; Override;
+    Function AsString(Const boolShowIdentifier, boolForDocumentation : Boolean) : String; Override;
   End;
 
 Implementation
@@ -36,13 +36,12 @@ uses
   @precon  None.
   @postcon Returns string representation of the Connection Definition.
 
-  @param   boolShowIdentifier   as a Boolean
-  @param   boolForDocumentation as a Boolean
+  @param   boolShowIdentifier   as a Boolean as a constant
+  @param   boolForDocumentation as a Boolean as a constant
   @return  a String
 
 **)
-function TConnectionDef.AsString(boolShowIdentifier,
-  boolForDocumentation: Boolean): String;
+Function TConnectionDef.AsString(Const boolShowIdentifier, boolForDocumentation : Boolean) : String;
 
 begin
   Result := '';

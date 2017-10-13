68138ed9cbf0f05d84f8da01f06982f1ff2be1cd
DGH2112 <davidghoyle@gmail.com>
2017-10-13 21:33:24 +0100

    Updated the signatures with CONST parameters.

--- Source/BADI.XML.BaseElement.pas
+++ Source/BADI.XML.BaseElement.pas
@@ -5,7 +5,7 @@
 
   @Author  David Hoyle
   @Version 1.0
-  @Date    12 Mar 2017
+  @Date    12 Oct 2017
 
 **)
 Unit BADI.XML.BaseElement;
@@ -25,11 +25,10 @@ Type
   {$IFDEF D2005} Strict {$ENDIF} Private
     FElementName: String;
   Public
-    Constructor Create(const strName : String; AScope : TScope; iLine,
-      iColumn : Integer; AImageIndex : TBADIImageIndex; AComment : TComment); Override;
+    Constructor Create(Const strName : String; Const AScope : TScope; Const iLine,
+      iColumn : Integer; Const AImageIndex : TBADIImageIndex; Const AComment : TComment); Override;
     Function GetName : String; Override;
-    Function AsString(boolShowIdenifier, boolForDocumentation : Boolean) : String;
-      Override;
+    Function AsString(Const boolShowIdenifier, boolForDocumentation : Boolean) : String; Override;
   End;
 
 Implementation
@@ -44,12 +43,12 @@ Uses
   @precon  None.
   @postcon Returns a string representation of the XML Base Element.
 
-  @param   boolShowIdenifier    as a Boolean
-  @param   boolForDocumentation as a Boolean
+  @param   boolShowIdenifier    as a Boolean as a constant
+  @param   boolForDocumentation as a Boolean as a constant
   @return  a String
 
 **)
-Function TXMLBaseElement.AsString(boolShowIdenifier, boolForDocumentation: Boolean): String;
+Function TXMLBaseElement.AsString(Const boolShowIdenifier, boolForDocumentation: Boolean): String;
 
 Begin
   Result := Identifier;
@@ -62,21 +61,24 @@ End;
   @precon  None.
   @postcon Initialises the class to be not sorted and have a unique name.
 
-  @param   strName     as a String as a Constant
-  @param   AScope      as a TScope
-  @param   iLine       as an Integer
-  @param   iColumn     as an Integer
-  @param   AImageIndex as a TBADIImageIndex
-  @param   AComment    as a TComment
+  @param   strName     as a String as a constant
+  @param   AScope      as a TScope as a constant
+  @param   iLine       as an Integer as a constant
+  @param   iColumn     as an Integer as a constant
+  @param   AImageIndex as a TBADIImageIndex as a constant
+  @param   AComment    as a TComment as a constant
 
 **)
-Constructor TXMLBaseElement.Create(Const strName: String; AScope: TScope; iLine, iColumn: Integer;
-  AImageIndex: TBADIImageIndex; AComment: TComment);
+Constructor TXMLBaseElement.Create(Const strName: String; Const AScope: TScope; Const iLine,
+  iColumn: Integer; Const AImageIndex: TBADIImageIndex; Const AComment: TComment);
+
+Const
+  strOutputFmt = '%s:%4.4d:%4.4d';
 
 Begin
   Inherited Create(strName, AScope, iLine, iColumn, AImageIndex, AComment);
   Sorted := False;
-  FElementName := Format('%s:%4.4d:%4.4d', [strName, iLine, iColumn]);
+  FElementName := Format(strOutputFmt, [strName, iLine, iColumn]);
 End;
 
 (**

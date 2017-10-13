cac1e2c84800ec86dd846e525eb56256f99ee4f8
DGH2112 <davidghoyle@gmail.com>
2017-10-13 21:34:19 +0100

    Updated the signatures with CONST parameters.

--- Source/BADI.BackusNaur.Rule.pas
+++ Source/BADI.BackusNaur.Rule.pas
@@ -4,7 +4,7 @@
 
   @Author  David Hoyle
   @Version 1.0
-  @Date    01 Apr 2017
+  @Date    12 Oct 2017
 
 **)
 Unit BADI.BackusNaur.Rule;
@@ -22,13 +22,13 @@ Type
   {$IFDEF D2005} Strict {$ENDIF} Private
   {$IFDEF D2005} Strict {$ENDIF} Protected
   Public
-    Function  AsString(boolShowIdenifier, boolForDocumentation : Boolean) : String;
-      Override;
+    Function  AsString(Const boolShowIdenifier, boolForDocumentation : Boolean) : String; Override;
   End;
 
 Implementation
 
 Uses
+  SysUtils,
   BADI.Options;
 
 (**
@@ -38,17 +38,20 @@ Uses
   @precon  None.
   @postcon Returns a string representation of the BNG Rule.
 
-  @param   boolShowIdenifier    as a Boolean
-  @param   boolForDocumentation as a Boolean
+  @param   boolShowIdenifier    as a Boolean as a constant
+  @param   boolForDocumentation as a Boolean as a constant
   @return  a String
 
 **)
-function TBackusNaurRule.AsString(boolShowIdenifier,
-  boolForDocumentation: Boolean): String;
-begin
-  Result := Name + ' ::= ' +
-    BuildStringRepresentation(False, boolForDocumentation, '::=',
-      BADIOptions.MaxDocOutputWidth, ['.', '+', '*'], ['.']);
-end;
+Function TBackusNaurRule.AsString(Const boolShowIdenifier, boolForDocumentation: Boolean): String;
+
+Const
+  strRulePrefix = ' ::= ';
+
+Begin
+  Result := Name + strRulePrefix +
+    BuildStringRepresentation(False, boolForDocumentation, Trim(strRulePrefix),
+    BADIOptions.MaxDocOutputWidth, ['.', '+', '*'], ['.']);
+End;
 
 End.

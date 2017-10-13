587ec011a81df01af0313db24cbc165354fdbc46
DGH2112 <davidghoyle@gmail.com>
2017-10-13 21:48:23 +0100

    Updated the signatures with CONST parameters.

--- Source/BADI.Eidolon.Association.pas
+++ Source/BADI.Eidolon.Association.pas
@@ -4,7 +4,7 @@
 
   @Author  David Hoyle
   @Version 1.0
-  @Date    12 Mar 2017
+  @Date    12 Oct 2017
 
 **)
 Unit BADI.Eidolon.Association;
@@ -21,7 +21,7 @@ Type
   TAssociation = Class(TElementContainer)
   {$IFDEF D2005} Strict {$ENDIF} Private
   Public
-    Function AsString(boolShowIdentifier, boolForDocumentation : Boolean) : String; Override;
+    Function AsString(Const boolShowIdentifier, boolForDocumentation : Boolean) : String; Override;
   End;
 
 Implementation
@@ -33,13 +33,12 @@ Implementation
   @precon  None.
   @postcon Returns a string representation of the class
 
-  @param   boolShowIdentifier   as a Boolean
-  @param   boolForDocumentation as a Boolean
+  @param   boolShowIdentifier   as a Boolean as a constant
+  @param   boolForDocumentation as a Boolean as a constant
   @return  a String
 
 **)
-function TAssociation.AsString(boolShowIdentifier,
-  boolForDocumentation: Boolean): String;
+Function TAssociation.AsString(Const boolShowIdentifier, boolForDocumentation : Boolean) : String;
 var
   iToken: Integer;
 begin

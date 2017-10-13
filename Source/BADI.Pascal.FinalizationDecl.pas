39ef712625c37afaaf265a34a290bcc4100a44e7
DGH2112 <davidghoyle@gmail.com>
2017-10-13 21:35:23 +0100

    Updated the signatures with CONST parameters.

--- Source/BADI.Pascal.FinalizationDecl.pas
+++ Source/BADI.Pascal.FinalizationDecl.pas
@@ -4,7 +4,7 @@
 
   @Author  David Hoyle
   @Version 1.0
-  @Date    01 Apr 2017
+  @Date    12 Oct 2017
 
 **)
 Unit BADI.Pascal.FinalizationDecl;
@@ -20,7 +20,7 @@ Type
   {$IFDEF D2005} Strict {$ENDIF} Private
   {$IFDEF D2005} Strict {$ENDIF} Protected
   Public
-    Function AsString(boolShowIdentifier, boolForDocumentation: Boolean): String; Override;
+    Function AsString(Const boolShowIdentifier, boolForDocumentation: Boolean): String; Override;
     Procedure CheckDocumentation(Var boolCascade: Boolean); Override;
   End;
 
@@ -34,17 +34,17 @@ Uses
 
 (**
 
-This is a getter method for the AsString property.
+  This is a getter method for the AsString property.
 
   @precon  None .
   @postcon Returns the name of the Finalisation section as a String .
 
-  @param   boolShowIdentifier   as a Boolean
-  @param   boolForDocumentation as a Boolean
+  @param   boolShowIdentifier   as a Boolean as a constant
+  @param   boolForDocumentation as a Boolean as a constant
   @return  a String
 
 **)
-Function TFinalizationSection.AsString(boolShowIdentifier, boolForDocumentation: Boolean): String;
+Function TFinalizationSection.AsString(Const boolShowIdentifier, boolForDocumentation: Boolean): String;
 Begin
   Result := '';
   If boolShowIdentifier Then

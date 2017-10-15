f37ddc0da3836eeb8d7c665b813d4c19a771e779
DGH2112 <davidghoyle@gmail.com>
2017-10-15 12:21:06 +0100

    Updated the signatures with CONST parameters.

--- Source/BADI.Eidolon.Rectangle.pas
+++ Source/BADI.Eidolon.Rectangle.pas
@@ -4,7 +4,7 @@
 
   @Author  David Hoyle
   @Version 1.0
-  @Date    12 Mar 2017
+  @Date    12 Oct 2017
 
 **)
 Unit BADI.Eidolon.Rectangle;
@@ -21,7 +21,7 @@ Type
   TRectangle = Class(TCustomFillSymbol)
   {$IFDEF D2005} Strict {$ENDIF} Private
   Public
-    Function AsString(boolShowIdentifier, boolForDocumentation: Boolean): String; Override;
+    Function AsString(Const boolShowIdentifier, boolForDocumentation: Boolean): String; Override;
   End;
 
 Implementation
@@ -36,13 +36,12 @@ Uses
   @precon  None.
   @postcon Returns a string representation of the class
 
-  @param   boolShowIdentifier   as a Boolean
-  @param   boolForDocumentation as a Boolean
+  @param   boolShowIdentifier   as a Boolean as a constant
+  @param   boolForDocumentation as a Boolean as a constant
   @return  a String
 
 **)
-Function TRectangle.AsString(boolShowIdentifier,
-  boolForDocumentation: Boolean): String;
+Function TRectangle.AsString(Const boolShowIdentifier, boolForDocumentation: Boolean): String;
 Begin
   Result := Inherited AsString(boolShowIdentifier, boolForDocumentation);
   If Transparency >= 0.0 Then

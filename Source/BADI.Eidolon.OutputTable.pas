4fbff2c7d229bc44ba0e24d862a3a9b50cbaf181
DGH2112 <davidghoyle@gmail.com>
2017-10-15 12:22:25 +0100

    Updated the signatures with CONST parameters.

--- Source/BADI.Eidolon.OutputTable.pas
+++ Source/BADI.Eidolon.OutputTable.pas
@@ -4,7 +4,7 @@
 
   @Author  David Hoyle
   @Version 1.0
-  @Date    12 Mar 2017
+  @Date    15 Oct 2017
 
 **)
 Unit BADI.Eidolon.OutputTable;
@@ -21,7 +21,7 @@ Type
   TOutputTable = Class(TDBTable)
   {$IFDEF D2005} Strict {$ENDIF} Private
   Public
-    Function AsString(boolShowIdentifier, boolForDocumentation : Boolean) : String; Override;
+    Function AsString(Const boolShowIdentifier, boolForDocumentation : Boolean) : String; Override;
   End;
 
 Implementation
@@ -33,15 +33,18 @@ Implementation
   @precon  None.
   @postcon Returns a string representation of the class
 
-  @param   boolShowIdentifier   as a Boolean
-  @param   boolForDocumentation as a Boolean
+  @param   boolShowIdentifier   as a Boolean as a constant
+  @param   boolForDocumentation as a Boolean as a constant
   @return  a String
 
 **)
-function TOutputTable.AsString(boolShowIdentifier,
-  boolForDocumentation: Boolean): String;
+Function TOutputTable.AsString(Const boolShowIdentifier, boolForDocumentation : Boolean) : String;
+
+Const
+  strTableType = '=Class(OutputTable)';
+
 begin
-  Result := Identifier + '=Class(OutputTable)';
+  Result := Identifier + strTableType;
 end;
 
 End.

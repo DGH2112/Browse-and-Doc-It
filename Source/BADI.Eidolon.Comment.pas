b15a15a739455dab2a839da86888b34caf4eb4a4
DGH2112 <davidghoyle@gmail.com>
2017-10-22 15:22:35 +0100

    Fixed incompatible signatures.

--- Source/BADI.Eidolon.Comment.pas
+++ Source/BADI.Eidolon.Comment.pas
@@ -4,7 +4,7 @@
 
   @Author  David Hoyle
   @Version 1.0
-  @Date    12 Mar 2017
+  @Date    22 Oct 2017
 
 **)
 Unit BADI.Eidolon.Comment;
@@ -20,34 +20,32 @@ Type
   (** A XML specific implementation of comments. **)
   TEidolonComment = Class(TComment)
   Public
-    Class Function CreateComment(const strComment: String; iLine,
-      iCol: Integer): TComment; Override;
+    Class Function CreateComment(const strComment: String; Const iLine, iCol: Integer): TComment;
+      Override;
   End;
 
 Implementation
 
 (**
 
+  This method is a class method to first check the comment for being a documentation comment and then 
+  creating an instance of a TComment class and parsing the comment via the constructor.
 
-  This method is a class method to first check the comment for being a
-  documentation comment and then creating an instance of a TComment class and
-  parsing the comment via the constructor.
-
-  @precon  strComment is the full comment to be checked and parsed, iLine is
-           the line number of the comment and iCol is the column number of
-           the comment.
-
-  @postcon Returns Nil if this is not a documentation comment or returns a
-           valid TComment class.
+  @precon  strComment is the full comment to be checked and parsed, iLine is the line number of the 
+           comment and iCol is the column number of the comment.
+  @postcon Returns Nil if this is not a documentation comment or returns a valid TComment class.
 
   @param   strComment as a String as a constant
-  @param   iLine      as an Integer
-  @param   iCol       as an Integer
+  @param   iLine      as an Integer as a constant
+  @param   iCol       as an Integer as a constant
   @return  a TComment
 
 **)
-class function TEidolonComment.CreateComment(const strComment: String; iLine,
+class function TEidolonComment.CreateComment(Const strComment: String; Const iLine,
   iCol: Integer): TComment;
+
+Const
+  iCommentPadding = 2;
 
 Var
   strText : String;
@@ -58,24 +56,24 @@ begin //: @note Not currently configured or used.
   If Length(strText) > 0 Then
     Begin
       Case strText[1] Of
-        '/' : strText := Copy(strText, 2, Length(strText) - 1);
+        '/' : strText := Copy(strText, iCommentPadding, Length(strText) - 1);
       End;
       If Length(strText) > 0 Then
         Begin
           If strText[1] = '*' Then
-            strText := Copy(strText, 2, Length(strText) - 3);
+            strText := Copy(strText, iCommentPadding, Length(strText) - iCommentPadding - 1);
           If strText[1] = '/' Then
-            strText := Copy(strText, 2, Length(strText) - 1);
+            strText := Copy(strText, iCommentPadding, Length(strText) - 1);
           If Length(strText) > 0 Then
             Begin
               If strText[1] = ':' Then
                 Begin;
-                  strText := Copy(strText, 2, Length(strText) - 1);
+                  strText := Copy(strText, iCommentPadding, Length(strText) - 1);
                   Result := Create(strText, iLine, iCol);
                 End
               Else If strText[1] = '*' Then
                 Begin;
-                  strText := Copy(strText, 2, Length(strText) - 2);
+                  strText := Copy(strText, iCommentPadding, Length(strText) - iCommentPadding);
                   Result := Create(strText, iLine, iCol);
                 End;
             End;

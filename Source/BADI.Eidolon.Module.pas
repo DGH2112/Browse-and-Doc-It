3b53159ca723f174ac83b30e317c5ea28405a8bf
DGH2112 <davidghoyle@gmail.com>
2017-10-27 21:03:44 +0100

    Added CONST to parameters.

--- Source/BADI.Eidolon.Module.pas
+++ Source/BADI.Eidolon.Module.pas
@@ -4,7 +4,7 @@
   "Eidolon Map File Grammar.bnf" for the complete grammar implemented.
 
   @Version    1.0
-  @Date       22 Oct 2017
+  @Date       27 Oct 2017
   @Author     David Hoyle
 
 **)
@@ -119,14 +119,13 @@ Type
     Procedure EatWhitespace;
     Function  EmptyLine : Boolean;
   {$IFDEF D2005} Strict {$ENDIF} Protected
-    Function GetComment(
-      CommentPosition : TCommentPosition = cpBeforeCurrentToken) : TComment;
+    Function GetComment(Const CommentPosition : TCommentPosition = cpBeforeCurrentToken) : TComment;
       Override;
     procedure TidyUpEmptyElements;
     Function GetModuleName : String; Override;
   Public
-    Constructor CreateParser(const Source, strFileName : String;
-      IsModified : Boolean; ModuleOptions : TModuleOptions); Override;
+    Constructor CreateParser(Const Source, strFileName : String; Const IsModified : Boolean;
+      Const ModuleOptions : TModuleOptions); Override;
     Destructor Destroy; Override;
     Function ReservedWords : TKeyWords; Override;
     Function Directives : TKeyWords; Override;
@@ -554,21 +553,19 @@ end;
 
   This is the constructor method for the TEidolonModule class.
 
-  @precon  Source is a valid TStream descendant containing as stream of text,
-           that is the contents of a source code module and Filename is the
-           file name of the module being parsed and IsModified determines if
-           the source code module has been modified since the last save to
-           disk.
+  @precon  Source is a valid TStream descendant containing as stream of text, that is the contents of a 
+           source code module and Filename is the file name of the module being parsed and IsModified 
+           determines if the source code module has been modified since the last save to disk.
   @postcon Creates an instance of the module parser.
 
   @param   Source        as a String as a constant
   @param   strFileName   as a String as a constant
-  @param   IsModified    as a Boolean
-  @param   ModuleOptions as a TModuleOptions
+  @param   IsModified    as a Boolean as a constant
+  @param   ModuleOptions as a TModuleOptions as a constant
 
 **)
-Constructor TEidolonModule.CreateParser(const Source, strFileName : String;
-  IsModified : Boolean; ModuleOptions : TModuleOptions);
+Constructor TEidolonModule.CreateParser(Const Source, strFileName : String; Const IsModified : Boolean;
+      Const ModuleOptions : TModuleOptions);
 
 Begin
   Inherited CreateParser(Source, strFileName, IsModified, ModuleOptions);
@@ -1706,21 +1703,20 @@ end;
 
 (**
 
-  This method tries to get a document comment from the previous token and return
-  a TComment class to the calling routine.
-
-  @note    All comments found are automatically added to the comment collection
-           for disposal when the parser is destroyed.
+  This method tries to get a document comment from the previous token and return a TComment class to the 
+  calling routine.
 
   @precon  None.
   @postcon Returns the comment immediately before the current token else nil.
 
-  @param   CommentPosition as a TCommentPosition
+  @note    All comments found are automatically added to the comment collection for disposal when the 
+           parser is destroyed.
+
+  @param   CommentPosition as a TCommentPosition as a constant
   @return  a TComment
 
 **)
-Function TEidolonModule.GetComment(
-  CommentPosition : TCommentPosition) : TComment;
+Function TEidolonModule.GetComment(Const CommentPosition : TCommentPosition) : TComment;
 
 Var
   T : TTokenInfo;

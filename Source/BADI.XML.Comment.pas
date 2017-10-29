1c4f2c5f578832da28601fb6d0d6f29688a20c19
DGH2112 <davidghoyle@gmail.com>
2017-10-29 10:30:33 +0000

    Updated comments with CONST parameters.

--- Source/BADI.XML.Comment.pas
+++ Source/BADI.XML.Comment.pas
@@ -4,7 +4,7 @@
 
   @Author  David Hoyle
   @Version 1.0
-  @Date    12 Mar 2017
+  @Date    29 Oct 2017
 
 **)
 Unit BADI.XML.Comment;
@@ -18,7 +18,7 @@ Type
   (** A XML specific implementation of comments. **)
   TXMLComment = Class(TComment)
   Public
-    Class Function CreateComment(const strComment: String; iLine, iCol: Integer): TComment;
+    Class Function CreateComment(Const strComment: String; Const iLine, iCol: Integer): TComment;
       Override;
   End;
 
@@ -26,27 +26,24 @@ Implementation
 
 (**
 
+  This method is a class method to first check the comment for being a documentation comment and then 
+  creating an instance of a TComment class and parsing the comment via the constructor.
 
-  This method is a class method to first check the comment for being a
-  documentation comment and then creating an instance of a TComment class and
-  parsing the comment via the constructor.
+  @precon  strComment is the full comment to be checked and parsed, iLine is the line number of the 
+           comment and iCol is the column number of the comment.
+  @postcon Returns Nil if this is not a documentation comment or returns a valid TComment class.
 
-  @precon  strComment is the full comment to be checked and parsed, iLine is
-           the line number of the comment and iCol is the column number of
-           the comment.
-
-  @postcon Returns Nil if this is not a documentation comment or returns a
-           valid TComment class.
-
-  @param   strComment as a String as a Constant
-  @param   iLine      as an Integer
-  @param   iCol       as an Integer
+  @param   strComment as a String as a constant
+  @param   iLine      as an Integer as a constant
+  @param   iCol       as an Integer as a constant
   @return  a TComment
 
 **)
-class function TXMLComment.CreateComment(const strComment: String; iLine,
-  iCol: Integer): TComment;
+class Function TXMLComment.CreateComment(Const strComment: String; Const iLine, iCol: Integer): TComment;
 
+Const
+  iSecondChar = 2;
+  
 Var
   strText : String;
 
@@ -56,24 +53,24 @@ begin //: @note Not currently configured or used.
   If Length(strText) > 0 Then
     Begin
       Case strText[1] Of
-        '/' : strText := Copy(strText, 2, Length(strText) - 1);
+        '/' : strText := Copy(strText, iSecondChar, Length(strText) - 1);
       End;
       If Length(strText) > 0 Then
         Begin
           If strText[1] = '*' Then
-            strText := Copy(strText, 2, Length(strText) - 3);
+            strText := Copy(strText, iSecondChar, Length(strText) - (iSecondChar + 1));
           If strText[1] = '/' Then
-            strText := Copy(strText, 2, Length(strText) - 1);
+            strText := Copy(strText, iSecondChar, Length(strText) - 1);
           If Length(strText) > 0 Then
             Begin
               If strText[1] = ':' Then
                 Begin;
-                  strText := Copy(strText, 2, Length(strText) - 1);
+                  strText := Copy(strText, iSecondChar, Length(strText) - 1);
                   Result := Create(strText, iLine, iCol);
                 End
               Else If strText[1] = '*' Then
                 Begin;
-                  strText := Copy(strText, 2, Length(strText) - 2);
+                  strText := Copy(strText, iSecondChar, Length(strText) - iSecondChar);
                   Result := Create(strText, iLine, iCol);
                 End;
             End;

3e54346ab32733850b8c852eacc27b435af66a91
DGH2112 <davidghoyle@gmail.com>
2017-10-29 10:34:50 +0000

    Updated comments with CONST parameters.

--- Source/BADI.Pascal.Comment.pas
+++ Source/BADI.Pascal.Comment.pas
@@ -4,7 +4,7 @@
 
   @Author  David Hoyle
   @Version 1.0
-  @Date    24 Feb 2017
+  @Date    29 Oct 2017
 
 **)
 Unit BADI.Pascal.Comment;
@@ -18,38 +18,39 @@ Type
   (** A pascal specific implementation of comments. **)
   TPascalComment = Class(TComment)
   Public
-    Class Function CreateComment(Const strComment: String; iLine, iCol: Integer): TComment;
+    Class Function CreateComment(Const strComment: String; Const iLine, iCol: Integer): TComment;
       Override;
   End;
 
 Implementation
 
-uses
-  BADI.Functions;
+Uses
+  BADI.Functions,
+  BADI.TYpes;
 
 (**
 
+  This method is a class method to first check the comment for being a documentation comment and then 
+  creating an instance of a TComment class and parsing the comment via the constructor.
 
-  This method is a class method to first check the comment for being a
-  documentation comment and then creating an instance of a TComment class and
-  parsing the comment via the constructor.
-
-  @precon  strComment is the full comment to be checked and parsed, iLine is
-           the line number of the comment and iCol is the column number of
-           the comment.
-
-  @postcon Returns Nil if this is not a documentation comment or returns a
-           valid TComment class.
+  @precon  strComment is the full comment to be checked and parsed, iLine is the line number of the 
+           comment and iCol is the column number of the comment.
+  @postcon Returns Nil if this is not a documentation comment or returns a valid TComment class.
 
   @param   strComment as a String as a constant
-  @param   iLine      as an Integer
-  @param   iCol       as an Integer
+  @param   iLine      as an Integer as a constant
+  @param   iCol       as an Integer as a constant
   @return  a TComment
 
 **)
-Class Function TPascalComment.CreateComment(Const strComment: String; iLine, iCol: Integer)
-  : TComment;
+Class Function TPascalComment.CreateComment(Const strComment: String; Const iLine,
+  iCol: Integer) : TComment;
 
+Const
+  iSecondChar = 2;
+  iThirdChar = 3;
+  iFourthChar = 4;
+  
 Var
   strText: String;
 
@@ -59,9 +60,9 @@ Begin
   If Length(strText) > 0 Then
     Begin
       Case strText[1] Of
-        '/': strText := Copy(strText, 3, Length(strText) - 2);
-        '{': strText := Copy(strText, 2, Length(strText) - 2);
-        '(': strText := Copy(strText, 3, Length(strText) - 4);
+        '/': strText := Copy(strText, iThirdChar, Length(strText) - iSecondChar);
+        '{': strText := Copy(strText, iSecondChar, Length(strText) - iSecondChar);
+        '(': strText := Copy(strText, iThirdChar, Length(strText) - iFourthChar);
       End;
       If Length(strText) > 0 Then
         Begin
@@ -71,7 +72,7 @@ Begin
             Begin
               If (IsInSet(strText[1], [':', '*'])) Then
                 Begin;
-                  strText := Copy(strText, 2, Length(strText) - 1);
+                  strText := Copy(strText, iSecondChar, Length(strText) - 1);
                   Result := Create(strText, iLine, iCol);
                 End;
             End;

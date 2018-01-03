b93e9063543261cdc8b8421db38c44d693236d7f
DGH2112 <davidghoyle@gmail.com>
2018-01-03 21:51:43 +0000

    Bracketed CodeSiteLogging with a CODESITE IFDEF.

--- Source/BADI.Initialisation.pas
+++ Source/BADI.Initialisation.pas
@@ -6,7 +6,7 @@
 
   @Author  David Hoyle
   @Version 1.0
-  @Date    02 Jan 2018
+  @Date    03 Jan 2018
 
 **)
 Unit BADI.Initialisation;
@@ -16,7 +16,7 @@ Interface
 Implementation
 
 Uses
-  {$IFDEF DEBUG}
+  {$IFDEF CODESITE}
   CodeSiteLogging,
   {$ENDIF}
   BADI.Types,

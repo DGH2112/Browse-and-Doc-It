0c4864490cd172cb3914802af9047c01f065aa2d
DGH2112 <davidghoyle@gmail.com>
2018-01-03 21:45:12 +0000

    BracketED CodeSiteLogging with a CODESITE IFDEF.

--- Source/BADI.Module.Checks.SubView.pas
+++ Source/BADI.Module.Checks.SubView.pas
@@ -4,7 +4,7 @@
 
   @Author  David Hoyle
   @Version 1.0
-  @Date    28 Dec 2017
+  @Date    03 Jan 2018
   
 **)
 Unit BADI.Module.Checks.SubView;
@@ -49,7 +49,7 @@ Type
 Implementation
 
 Uses
-  {$IFDEF DEBUG}
+  {$IFDEF CODESITE}
   CodeSiteLogging,
   {$ENDIF}
   SysUtils,

5ec881ad42942eda16ed540681aa77d23e04dc0b
DGH2112 <davidghoyle@gmail.com>
2020-03-07 10:14:27 +0000

    Added GPL3 license text to the unit headers

--- Source/BADI.DocIssuesHintWindow.pas
+++ Source/BADI.DocIssuesHintWindow.pas
@@ -3,9 +3,29 @@
   This module contains a custom hint window to display the module metric totals.
 
   @Author  David Hoyle
-  @Version 1.286
-  @Date    08 Feb 2020
+  @Version 1.374
+  @Date    09 Feb 2020
   
+  @license
+
+    Browse and Doc It is a RAD Studio plug-in for browsing, checking and
+    documenting your code.
+    
+    Copyright (C) 2019  David Hoyle (https://github.com/DGH2112/Browse-and-Doc-It/)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 **)
 Unit BADI.DocIssuesHintWindow;
 

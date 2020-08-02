aa48199e2cfa88e741925074d86b1f0af3e0c40c
DGH2112 <davidghoyle@gmail.com>
2020-08-02 14:35:58 +0100

    Refactored the editor drawing code

--- Source/BADI.EditViewNotifier.pas
+++ Source/BADI.EditViewNotifier.pas
@@ -3,7 +3,7 @@
   This module contains a class which implements the INTAEditViewNotifier for drawing on the editor.
 
   @Author  David Hoyle
-  @Version 5.516
+  @Version 6.139
   @Date    02 Aug 2020
   
   @license
@@ -11,7 +11,7 @@
     Browse and Doc It is a RAD Studio plug-in for browsing, checking and
     documenting your code.
     
-    Copyright (C) 2019  David Hoyle (https://github.com/DGH2112/Browse-and-Doc-It/)
+    Copyright (C) 2020  David Hoyle (https://github.com/DGH2112/Browse-and-Doc-It/)
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -86,6 +86,10 @@ Type
     Procedure HighlightSpellingMistakes(Const Canvas : TCanvas;
       Const LineDocIssues : IBADILineDocIssues; Const LineText: PAnsiChar; Const TextRect : TRect;
       Const CellSize: TSize);
+      procedure RenderIcons(const recDocIssue: TBADIDocIssueInfo; const Canvas:
+          TCanvas; const TextRect, LineRect: TRect; var R: TRect);
+    Procedure RenderMsgs(Const recDocIssue: TBADIDocIssueInfo; Const Canvas: TCanvas;
+      Const TextRect: TRect; Const CellSize: TSize; Var R: TRect);
   Public
     Constructor Create;
     Destructor Destroy; Override;
@@ -129,9 +133,30 @@ Const
 **)
 Procedure TBADIEditViewNotifier.BeginPaint(Const View: IOTAEditView; Var FullRepaint: Boolean);
 
+Const
+  aRenderIconInfo : Array[TLimitType] Of TDocOption = (
+    doShowErrorIconsInEditor,
+    doShowWarningIconsInEditor,
+    doShowHintIconsInEditor,
+    doShowConflictIconsInEditor,
+    doShowCheckIconsInEditor,
+    doShowMetricIconsInEditor,
+    doShowSpellingIconsInEditor
+  );
+  aRenderMsgInfo : Array[TLimitType] Of TDocOption = (
+    doShowErrorMsgsInEditor,
+    doShowWarningMsgsInEditor,
+    doShowHintMsgsInEditor,
+    doShowConflictMsgsInEditor,
+    doShowCheckMsgsInEditor,
+    doShowMetricMsgsInEditor,
+    doShowSpellingMsgsInEditor
+  );
+  
 Var
   DocOps: TDocOptions;
   iTag: Integer;
+  eLimitType: TLimitType;
 
 Begin
   {$IFDEF CODESITE}CodeSite.TraceMethod(Self, 'BeginPaint', tmoTiming);{$ENDIF}
@@ -144,27 +169,15 @@ Begin
   If FTokenFontInfo.FForeColour = clNone Then
     FTokenFontInfo.FForeColour := FPlainTextFontInfo.FForeColour;
   DocOps := TBADIOptions.BADIOptions.Options;
-  //: @refactor Needs to be based on enumerate so it updated automatically
   FIconsToRender.Clear;
-  IconsToRender(DocOps, doShowErrorIconsInEditor, ltErrors);
-  IconsToRender(DocOps, doShowWarningIconsInEditor, ltWarnings);
-  IconsToRender(DocOps, doShowHintIconsInEditor, ltHints);
-  IconsToRender(DocOps, doShowConflictIconsInEditor, ltConflicts);
-  IconsToRender(DocOps, doShowCheckIconsInEditor, ltChecks);
-  IconsToRender(DocOps, doShowMetricIconsInEditor, ltMetrics);
-  IconsToRender(DocOps, doShowSpellingIconsInEditor, ltSpelling);
+  For eLimitType := Low(TLimitType) To High(TLimitType) Do
+    IconsToRender(DocOps, aRenderIconInfo[eLimitType], eLimitType);
   For iTag := 0 To TBADIOptions.BADIOptions.SpecialTags.Count - 1 Do
     If tpShowInEditor In TBADIOptions.BADIOptions.SpecialTags[iTag].FTagProperties Then
       FIconsToRender.Add(TBADIOptions.BADIOptions.SpecialTags[iTag].FName);
-  //: @refactor Needs to be based on enumerate so it updated automatically
   FMsgsToRender.Clear;
-  MsgsToRender(DocOps, doShowErrorMsgsInEditor, ltErrors);
-  MsgsToRender(DocOps, doShowWarningMsgsInEditor, ltWarnings);
-  MsgsToRender(DocOps, doShowHintMsgsInEditor, ltHints);
-  MsgsToRender(DocOps, doShowConflictMsgsInEditor, ltConflicts);
-  MsgsToRender(DocOps, doShowCheckMsgsInEditor, ltChecks);
-  MsgsToRender(DocOps, doShowMetricMsgsInEditor, ltMetrics);
-  //: @debug MsgsToRender(DocOps, doShowSpellingMsgsInEditor, ltSpelling);
+  For eLimitType := Low(TLimitType) To High(TLimitType) Do
+    MsgsToRender(DocOps, aRenderMsgInfo[eLimitType], eLimitType);
   FindHorizontalScrollPosition(View);
 End;
 
@@ -554,7 +567,6 @@ Var
   LineDocIssues : IBADILineDocIssues;
   strDocIssue: String;
   recDocIssue : TBADIDocIssueInfo;
-  iIndex: Integer;
   
 Begin
   {$IFDEF CODESITE}CodeSite.TraceMethod(Self, 'PaintLine', tmoTiming);{$ENDIF}
@@ -564,28 +576,79 @@ Begin
     For strDocIssue In LineDocIssues.Issues Do
       Begin
         recDocIssue := LineDocIssues[strDocIssue];
-        If FIconsToRender.Find(recDocIssue.FName, iIndex) Or (recDocIssue.FImageIndex In [iiBadTag]) Then
-          Begin
-            TBADIOptions.BADIOptions.ScopeImageList.Draw(
-              Canvas,
-              R.Left,
-              R.Top,
-              BADIImageIndex(recDocIssue.FImageIndex, scNone)
-            );
-            Inc(R.Left, TBADIOptions.BADIOptions.ScopeImageList.Width + iPadding);
-            // After first icon change to Text Rect
-            If (R.Left > LineRect.Left) And (R.Left < TextRect.Right) Then
-              R.Left := TextRect.Right + iPadding;
-          End;
-        If FMsgsToRender.Find(recDocIssue.FName, iIndex) Then
-          Begin
-            DrawMsgText(Canvas, R, recDocIssue.FMessage, recDocIssue);
-            Inc(R.Left, iPadding);
-          End;
+        RenderIcons(recDocIssue, Canvas, TextRect, LineRect, R);
+        RenderMsgs(recDocIssue, Canvas, TextRect, CellSize, R);
         MarkUpdateSpecialTags(Canvas, strDocIssue, recDocIssue, LineText, TextRect, CellSize);
         If CompareText(strDocIssue, astrLimitType[ltSpelling]) = 0 Then
           HighlightSpellingMistakes(Canvas, LineDocIssues, LineText, TextRect, CellSize);
      End;
+End;
+
+(**
+
+  This method renders the icons for the given document issue in the editor for the current line.
+
+  @precon  Canvas must be a valid instance.
+  @postcon The first icon render it in the left hand margin but all subsequent icons are rendered to the
+           right of the editor text.
+
+  @param   recDocIssue as a TBADIDocIssueInfo as a constant
+  @param   Canvas      as a TCanvas as a constant
+  @param   TextRect    as a TRect as a constant
+  @param   LineRect    as a TRect as a constant
+  @param   R           as a TRect as a reference
+
+**)
+Procedure TBADIEditViewNotifier.RenderIcons(Const recDocIssue: TBADIDocIssueInfo; Const Canvas: TCanvas;
+  Const TextRect, LineRect: TRect; Var R: TRect);
+
+Var
+  iIndex: Integer;
+  
+Begin
+  If FIconsToRender.Find(recDocIssue.FName, iIndex) Or (recDocIssue.FImageIndex In [iiBadTag]) Then
+    Begin
+      TBADIOptions.BADIOptions.ScopeImageList.Draw(
+        Canvas,
+        R.Left,
+        R.Top,
+        BADIImageIndex(recDocIssue.FImageIndex, scNone)
+        );
+      Inc(R.Left, TBADIOptions.BADIOptions.ScopeImageList.Width + iPadding);
+      // After first icon change to Text Rect
+      If (R.Left > LineRect.Left) And (R.Left < TextRect.Right) Then
+        R.Left := TextRect.Right + iPadding;
+    End;
+End;
+
+(**
+
+  This method renders the message for the given document issue in the editor for the current line.
+
+  @precon  Canvas must be a valid instance.
+  @postcon All messages are rendered to the right of the text in the editor.
+
+  @param   recDocIssue as a TBADIDocIssueInfo as a constant
+  @param   Canvas      as a TCanvas as a constant
+  @param   TextRect    as a TRect as a constant
+  @param   CellSize    as a TSize as a constant
+  @param   R           as a TRect as a reference
+
+**)
+Procedure TBADIEditViewNotifier.RenderMsgs(Const recDocIssue: TBADIDocIssueInfo;
+  Const Canvas: TCanvas; Const TextRect: TRect; Const CellSize: TSize; Var R: TRect);
+
+Var
+  iIndex: Integer; 
+  
+Begin
+  If FMsgsToRender.Find(recDocIssue.FName, iIndex) Then
+    Begin
+      If R.Left < TextRect.Right Then
+        R.Left := TextRect.Right + CellSize.cx;
+      DrawMsgText(Canvas, R, recDocIssue.FMessage, recDocIssue);
+      Inc(R.Left, iPadding);
+    End;
 End;
 
 {$ENDIF DXE100}
